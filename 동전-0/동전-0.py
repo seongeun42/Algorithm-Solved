43d6f53f5ccf8c149a077d6de7b52a903162f25d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-01 22:44:20 +0900

    Update 동전-0.py

--- 동전-0/동전-0.py
+++ 동전-0/동전-0.py
@@ -2,9 +2,8 @@ import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
 coin = [int(input()) for _ in range(n)]
-coin = coin[::-1]
 cnt = 0
-for v in coin:
+for v in coin[::-1]:
   if not k:
     break
   if v > k:
