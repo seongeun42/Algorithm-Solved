6dabba307cbb3a7a30c08bc3a04507b92ff69d88
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-04 12:52:01 +0900

    Title: 에라토스테네스의-체, Time: 72 ms, Memory: 29200 KB

--- 에라토스테네스의-체/에라토스테네스의-체.py
+++ 에라토스테네스의-체/에라토스테네스의-체.py
@@ -1,7 +1,6 @@
 n, k = map(int, input().split())
 nums = [True] * (n + 1)
 for i in range(2, n + 1):
-  if nums[i]:
     for j in range(i, n + 1, i):
       if nums[j]:
         nums[j] = False
@@ -9,5 +8,3 @@ for i in range(2, n + 1):
         if not k:
           print(j)
           break
-  if not k:
-    break
