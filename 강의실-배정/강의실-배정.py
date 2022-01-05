ba91b19cdc787dcb52013de6487ca42dc55a9925
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-05 23:21:01 +0900

    [Gold V] Title: 강의실-배정, Time: 448 ms, Memory: 73968 KB -BaekjoonHub

---
+++ 강의실-배정/강의실-배정.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = [list(map(int, input().split())) for _ in range(n)]
+q.sort(key=lambda x: x[0])
+room = []
+heapq.heappush(room, q[0][1])
+
+for i in range(1, n):
+    if q[i][0] < room[0]:
+        heapq.heappush(room, q[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, q[i][1])
+
+print(len(room))
