1ff7dff3da574329413d7add8271007bd9ac2010
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-05 23:43:17 +0900

    [Silver I] Title: 최소-힙, Time: 140 ms, Memory: 36788 KB -BaekjoonHub

---
+++ 최소-힙/최소-힙.py
@@ -0,0 +1,11 @@
+import heapq
+import sys
+input = sys.stdin.readline
+n = int(input())
+hq = []
+for _ in range(n):
+    v = int(input())
+    if not v:
+        print(heapq.heappop(hq) if hq else 0)
+    else:
+        heapq.heappush(hq, v)
