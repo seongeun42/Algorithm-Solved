0e125ec2ab306c50df4bb69b2519241a7b00a46e
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-10 00:23:29 +0900

    [Silver II] Title: 카드-합체-놀이, Time: 276/92 ms, Memory: 30864/32932 KB

--- 카드-합체-놀이/카드-합체-놀이.py
+++ 카드-합체-놀이/카드-합체-놀이.py
@@ -1,6 +1,18 @@
 n, m = map(int, input().split())
 c = list(map(int, input().split()))
+
+
+# 정렬로 푼 코드
 for _ in range(m):
   c = sorted(c)
   c[0], c[1] = c[0] + c[1], c[0] + c[1]
+print(sum(c))
+
+
+# 우선순위큐로 푼 코드
+hq.heapify(c)
+for _ in range(m):
+  v1, v2 = hq.heappop(c), hq.heappop(c)
+  hq.heappush(c, v1 + v2)
+  hq.heappush(c, v1 + v2)
 print(sum(c))
