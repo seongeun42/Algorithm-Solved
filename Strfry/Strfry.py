cbe8f1b54b80b606a6052d5a376c26a35129bd0a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-12 19:51:26 +0900

    [Bronze II] Title: Strfry, Time: 176 ms, Memory: 30860 KB -BaekjoonHub

--- Strfry/Strfry.py
+++ Strfry/Strfry.py
@@ -3,11 +3,4 @@ input = sys.stdin.readline
 n = int(input())
 for _ in range(n):
   s1, s2 = input().split()
-  s1 = sorted(list(s1))
-  s2 = sorted(list(s2))
-  flag = True
-  for i in range(len(s1)):
-    if s1[i] != s2[i]:
-      flag = False
-      break
-  print("Possible" if flag else "Impossible")
+  print("Possible" if sorted(s1) == sorted(s2) else "Impossible")
