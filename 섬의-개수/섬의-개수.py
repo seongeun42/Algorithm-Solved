6e01901dcdc4445f77d788122232c6f8802b2c05
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-13 21:53:18 +0900

    [Silver II] Title: 섬의-개수, Time: 116 ms, Memory: 32444 KB -BaekjoonHub

--- 섬의-개수/섬의-개수.py
+++ 섬의-개수/섬의-개수.py
@@ -3,22 +3,22 @@ import sys
 input = sys.stdin.readline
 
 def bfs(jd, x, y):
-    dx = [0, 1, 1, 1, 0, -1, -1, -1]
-    dy = [-1, -1, 0, 1, 1, 1, 0, -1]
     q = deque([[x, y]])
     jd[y][x] = 0
     while q:
         l = q.popleft()
+        dx = [l[0], l[0] + 1, l[0] + 1, l[0] + 1, l[0], l[0] - 1, l[0] - 1, l[0] - 1]
+        dy = [l[1] - 1, l[1] - 1, l[1], l[1] + 1, l[1] + 1, l[1] + 1, l[1], l[1] - 1]
         for n in range(8):
-            if l[0] + dx[n] >= 0 and l[0] + dx[n] < len(jd[0]):
-                if l[1] + dy[n] >= 0 and l[1] + dy[n] < len(jd):
-                    if jd[l[1] + dy[n]][l[0] + dx[n]] == 1:
-                        jd[l[1] + dy[n]][l[0] + dx[n]] = 0
-                        q.append([l[0] + dx[n], l[1] + dy[n]])
+            if 0 <= dx[n] < len(jd[0]) and 0 <= dy[n] < len(jd):
+                if jd[dy[n]][dx[n]] == 1:
+                    jd[dy[n]][dx[n]] = 0
+                    q.append([dx[n], dy[n]])
     return 1
 
-n, m = map(int, input().split())
-while n != 0 or m != 0:
+while 1:
+    n, m = map(int, input().split())
+    if n == m == 0: break
     jido = [list(map(int, input().split())) for _ in range(m)]
     cnt = 0
     for i in range(m):
@@ -26,4 +26,3 @@ while n != 0 or m != 0:
             if jido[i][j] == 1:
                 cnt += bfs(jido, j, i)
     print(cnt)
-    n, m = map(int, input().split())
