19d754ef95b45cd646024cf6055a961d337cfb43
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-16 19:15:33 +0900

    백준허브 업데이트

--- 로프/로프.py
+++
@@ -1,8 +0,0 @@
-import sys
-input = sys.stdin.readline
-n = int(input())
-lope = sorted([int(input()) for _ in range(n)], reverse=True)
-w = 0
-for i in range(n):
-  w = max(w, lope[i] * (i + 1))
-print(w)
