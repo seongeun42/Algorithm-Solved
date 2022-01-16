1cacd49fc8229e2a804e2a6784c9460bccb46b74
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-16 19:16:02 +0900

    백준허브 업데이트

--- 파도반-수열/파도반-수열.py
+++
@@ -1,7 +0,0 @@
-t = int(input())
-p = [0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
-for _ in range(t):
-    n = int(input())
-    for i in range(len(p), n + 1):
-        p.append(p[i - 1] + p[i - 5])
-    print(p[n])
