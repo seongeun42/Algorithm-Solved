1cb476ec42b196c6c0f6a9082b95278343d5aa90
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-19 12:12:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 34712 KB -BaekjoonHub

--- 백준/Silver/1697.숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697.숨바꼭질/숨바꼭질.py
@@ -1,22 +1,17 @@
-from collections import deque
-def bfs(check, start, target):
-    check[start] = 0
-    q = deque([start])
-    while q:
-        v = q.popleft()
-        if v * 2 <= target + 1 and check[v * 2] == -1:
-            check[v * 2] = check[v] + 1
-            q.append(v * 2)
-        if v > 0 and check[v - 1] == -1:
-            check[v - 1] = check[v] + 1
-            q.append(v - 1)
-        if v <= target and check[v + 1] == -1:
-            check[v + 1] = check[v] + 1
-            q.append(v + 1)
+if __name__ == '__main__':
+    n, k = map(int, input().split())
+    dp = [0] * 100005
+    for i in range(n):
+        dp[i] = n - i
 
-n, k = map(int, input().split())
-if n >= k: print(n - k)
-else:
-    check = [-1] * (k + 2)
-    bfs(check, n, k)
-    print(check[k])
+    temp = n + 1
+    if n % 2 == 0:
+        dp[temp] = 1
+        temp += 1
+
+    for i in range(temp, k+3, 2):
+        dp[i] = 1 + min(dp[i//2], dp[i-1])
+        dp[i-1] = min(dp[i]+1, dp[i-1])
+        dp[i+1] = dp[i] + 1
+
+    print(dp[k])
