c2eff07dcecfd877bd0aa5f88762711e278564bc
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-20 07:29:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 31104 KB -BaekjoonHub

--- 백준/Silver/2667.단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667.단지번호붙이기/단지번호붙이기.py
@@ -1,12 +1,30 @@
+from collections import deque
+
 def dfs(jib, x, y):
     jib[y][x], c = 0, 1
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
-    for i in range(4):
-        l, m = x + dx[i], y + dy[i]
-        if l >= 0 and l < len(jib) and m >= 0 and m < len(jib):
+    for k in range(4):
+        l, m = x + dx[k], y + dy[k]
+        if 0 <= l < len(jib) and 0 <= m < len(jib):
             if jib[m][l]:
                 c += dfs(jib, l, m)
+    return c
+
+def bfs(jib, x, y):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    jib[y][x], c = 0, 1
+    q = deque([[x, y]])
+    while q:
+        v = q.popleft()
+        for k in range(4):
+            l, m = v[0] + dx[k], v[1] + dy[k]
+            if 0 <= l < len(jib) and 0 <= m < len(jib):
+                if jib[m][l]:
+                    jib[m][l] = 0
+                    q.append([l, m])
+                    c += 1
     return c
 
 n = int(input())
@@ -16,5 +34,6 @@ for i in range(n):
     for j in range(n):
         if jib[i][j] == 1:
             cnt.append(dfs(jib, j, i))
+            # cnt.append(bfs(jib, j, i))
 cnt.sort()
 print(len(cnt), *cnt, sep='\n')
