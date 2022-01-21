5163355f768a0029f946107faa45e11908e09d34
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-22 00:40:16 +0900

    [Silver IV] Title: Four Squares, Time: 104 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/17626.Four Squares/Four Squares.py
+++ 백준/Silver/17626.Four Squares/Four Squares.py
@@ -1,11 +1,16 @@
-n = int(input())
-dp = [0] * (n + 1)
-dp[1] = 1
-for i in range(2, n + 1):
-  mv = 1e9
-  j = 1
-  while j**2 <= i:
-    mv = min(mv, dp[i - j**2])
-    j += 1
-  dp[i] = mv + 1
-print(dp[n])
+def find(n):
+  v = n ** 0.5
+  if int(v) == v:
+    return 1
+  for i in range(int(v) + 1):
+    k = n - i ** 2
+    if int(k ** 0.5) == k ** 0.5:
+      return 2
+  for i in range(int(v) + 1):
+    k = n - i ** 2
+    for j in range(int(k ** 0.5) + 1):
+      if int((k - j ** 2) ** 0.5) == (k - j ** 2) ** 0.5:
+        return 3
+  return 4
+
+print(find(int(input())))
