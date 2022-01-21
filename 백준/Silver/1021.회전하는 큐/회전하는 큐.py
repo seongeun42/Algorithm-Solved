63ac7beb1609d9d23bcdf21bfc66ed803dddfac2
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-22 00:33:05 +0900

    [Silver IV] Title: 회전하는 큐, Time: 88 ms, Memory: 32368 KB -BaekjoonHub

--- 백준/Silver/1021.회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021.회전하는 큐/회전하는 큐.py
@@ -1,10 +1,19 @@
-import sys
-n,m = map(int,sys.stdin.readline().split())
-circularq = [i for i in range(1,n+1)]
-location = list(map(int,sys.stdin.readline().split()))
-result = 0
-for i in location:
-    index = circularq.index(i)
-    result += min(index,len(circularq)-index)
-    circularq = circularq[index+1:]+circularq[:index]
-print(result)
+from collections import deque
+n, m = map(int, input().split())
+s = list(map(int, input().split()))
+q = deque(list(range(1, n + 1)))
+cnt = 0
+for i in range(m):
+    loca = q.index(s[i])
+    if loca > len(q) // 2:
+        while q[-1] != s[i]:
+            q.appendleft(q.pop())
+            cnt += 1
+        q.pop()
+        cnt += 1
+    else:
+        while q[0] != s[i]:
+            q.append(q.popleft())
+            cnt += 1
+        q.popleft()
+print(cnt)
