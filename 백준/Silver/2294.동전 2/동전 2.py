deb05e363d22eede9ab1ddd104389f9893e7eed8
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-22 21:13:52 +0900

    [Silver I] Title: 동전 2, Time: 304 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/2294.동전 2/동전 2.py
+++ 백준/Silver/2294.동전 2/동전 2.py
@@ -1,13 +1,32 @@
-n, k = map(int, input().split())
-coin = list(int(input()) for _ in range(n))
-dp = [0] * (k + 1)
-for i in range(1, k + 1):
-    l = []
-    for v in coin:
-        if v <= i and dp[i - v] != -1:
-            l.append(dp[i - v])
-    if l:
-        dp[i] = min(l) + 1
-    else:
-        dp[i] = -1
-print(dp[k])
+#동전 개수 및 가치의 합
+n, k = map(int,input().split())
+
+coin_list = [-1 for i in range(k+1)]
+A = []
+
+#동전 입력 n번
+
+coin = 0
+for i in range(0,n):
+    coin = int(input())
+    A.append(coin)
+
+A.sort()
+index = 0
+for i in range(1,k+1):
+  if(index<n-1):
+    if(i-A[index+1]>=0):
+      index = index + 1
+  if(i-A[index]==0):
+    coin_list[i] = 1
+  else:
+    min = 1000000
+    for k1 in range(0,index+1):
+      temp = coin_list[i-A[index-k1]]
+      #print(temp)
+      if(temp<min and temp!=-1):
+        min = temp
+    if(min!=1000000):
+        coin_list[i] = min+1
+    
+print(coin_list[k])
