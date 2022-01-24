fac986511fe186bbc1f892ea26b8bd2a9680de18
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-25 00:52:53 +0900

    [Silver III] Title: 게임, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/1072.게임/게임.py
+++ 백준/Silver/1072.게임/게임.py
@@ -1,13 +1,18 @@
-x, y = map(int, input().split())
-z = (y * 100) // x
-if z >= 99: print(-1)
-else:
-    s, e = 0, 10**9
-    while s <= e:
-        m = (s + e) // 2
-        v = ((y + m) * 100) // (x + m)
-        if v > z:
-            e = m - 1
+X, Y = map(int, input().split())
+
+win = Y * 100 // X
+
+answer = 0
+if win < 99:
+    l, r = 1, X
+    while l <= r:
+        m = (l + r) // 2
+
+        temp = (Y + m) * 100 // (X + m)
+        if temp > win:
+            r = m - 1
+            answer = m
         else:
-            s = m + 1
-    print(e + 1)
+            l = m + 1
+
+print(answer if answer > 0 else -1)
