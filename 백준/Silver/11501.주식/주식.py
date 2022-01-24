f149b2380631fe953f9728156038debb7ec5edde
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-24 23:46:42 +0900

    [Silver II] Title: 주식, Time: 2508 ms, Memory: 167540 KB -BaekjoonHub

--- 백준/Silver/11501.주식/주식.py
+++ 백준/Silver/11501.주식/주식.py
@@ -1,15 +1,19 @@
-import sys
-input = sys.stdin.readline
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    p = list(map(int, input().split()))[::-1]
-    s, mp, res = [], -1e9, 0
-    for v in p:
-        if v > mp:
-            res += sum([mp - k for k in s])
-            mp = v
-            s = []
-        elif v < mp:
-            s.append(v)
-    print(res + sum([mp - k for k in s]))
+def solution(stock_list):
+    res = 0
+    max_profit = stock_list[-1]
+    stock_list = stock_list[::-1]
+
+    for val in stock_list:
+        if val > max_profit:
+            max_profit = val
+        else:
+            res += max_profit - val
+
+    return res
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    stock = list(map(int, input().split()))
+    print(solution(stock))
