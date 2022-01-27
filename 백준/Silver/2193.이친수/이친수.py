f2ecf45875044a44800961e2d42097d82c9236b8
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-27 17:06:27 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/2193.이친수/이친수.py
+++ 백준/Silver/2193.이친수/이친수.py
@@ -1,7 +1,5 @@
-n = int(input())
-dp = [[0, 0] for _ in range(n + 1)]
-dp[1] = [0, 1]
-for i in range(2, n + 1):
-    dp[i][0] = dp[i - 1][0] + dp[i - 1][1]
-    dp[i][1] = dp[i - 1][0]
-print(sum(dp[n]))
+N = int(input())
+a = [0, 1, 1]
+for i in range(3, 91):
+    a.append(a[i-1]+a[i-2])
+print(a[N])
