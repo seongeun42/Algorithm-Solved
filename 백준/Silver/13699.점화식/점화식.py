a224ebbbd34239da90071611148d2f7eee3f8ac3
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-27 16:47:08 +0900

    [Silver IV] Title: 점화식, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

--- 백준/Silver/13699.점화식/점화식.py
+++ 백준/Silver/13699.점화식/점화식.py
@@ -1,7 +1,9 @@
-n = int(input())
-dp = [0] * 36
-dp[0] = 1
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        dp[i] += dp[j - 1] * dp[i - j]
-print(dp[n])
+dp=[0]*36
+dp[0]=1
+
+N= int(input())
+
+for i in range(1,N+1):
+    for j in range(i):
+        dp[i]+=dp[j]*dp[i-j-1]
+print(dp[N])
