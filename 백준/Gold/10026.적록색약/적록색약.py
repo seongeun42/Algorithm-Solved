cabb751736051a1e36b1f8fac767b9ed53e9cc25
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-01-30 10:21:13 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 33592 KB -BaekjoonHub

--- 백준/Gold/10026.적록색약/적록색약.py
+++ 백준/Gold/10026.적록색약/적록색약.py
@@ -1,31 +1,55 @@
-import sys
-sys.setrecursionlimit(10**7)
-input = sys.stdin.readline
+from sys import stdin, setrecursionlimit
+input = stdin.readline
+setrecursionlimit(10000)
 
-def dfs(p, x, y):
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    c = p[y][x]
-    p[y][x] = 0
-    for i in range(4):
-        a, b = x + dx[i], y + dy[i]
-        if 0 <= a < n and 0 <= b < n:
-            if p[b][a] == c:
-                dfs(p, a, b)
-    return 1
+N = int(input())
+arr = [list(input().strip()) for _ in range(N)]
 
-n = int(input())
-pic = [list(input().strip()) for _ in range(n)]
-pic_p = []
-for l in pic:
-    pic_p.append([])
-    for v in l:
-        pic_p[-1].append('R' if v == 'G' else v)
-cnt, cnt_p = 0, 0
-for i in range(n):
-    for j in range(n):
-        if pic[i][j] != 0:
-            cnt += dfs(pic, j, i)
-        if pic_p[i][j] != 0:
-            cnt_p += dfs(pic_p, j, i)
-print(cnt, cnt_p, sep=' ')
+def dfs(x, y, color, changeTo):
+  if arr[x][y] == color:
+    arr[x][y] = changeTo
+    if x - 1 >= 0 and arr[x - 1][y] == color:
+      dfs(x - 1, y, color, changeTo)
+    if x + 1 < N and arr[x + 1][y] == color:
+      dfs(x + 1, y, color, changeTo)
+    if y - 1 >= 0 and arr[x][y - 1] == color:
+      dfs(x, y - 1, color, changeTo)
+    if y + 1 < N and arr[x][y + 1] == color:
+      dfs(x, y + 1, color, changeTo)
+
+answer = [0, 0]
+cnt = 0
+for i in range(N):
+  for j in range(N):
+    if arr[i][j] == 'G':
+      dfs(i, j, 'G', 0)
+      cnt += 1
+answer[0] += cnt
+
+cnt = 0
+for i in range(N):
+  for j in range(N):
+    if arr[i][j] == 'R':
+      dfs(i, j, 'R', 0)
+      cnt += 1
+answer[0] += cnt
+
+cnt = 0
+for i in range(N):
+  for j in range(N):
+    if arr[i][j] == 0:
+      dfs(i, j, 0, 1)
+      cnt += 1
+answer[1] += cnt
+
+cnt = 0
+for i in range(N):
+  for j in range(N):
+    if arr[i][j] == 'B':
+      dfs(i, j, 'B', 1)
+      cnt += 1
+answer[1] += cnt
+answer[0] += cnt
+
+for a in answer:
+  print(a, end=' ')
