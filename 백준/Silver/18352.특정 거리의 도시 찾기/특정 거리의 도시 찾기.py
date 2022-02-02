48fb0438eaa7ed8f563806810060f92473c81eae
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-02 15:21:44 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1296 ms, Memory: 100280 KB -BaekjoonHub

--- 백준/Silver/18352.특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352.특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,27 +1,41 @@
-from collections import deque
 import sys
+import heapq
+
 input = sys.stdin.readline
 
-def bfs(node):
-    q = deque([node])
-    res[node] = 0
-    while q:
-        v = q.popleft()
-        for i in m[v]:
-            if res[i] == -1:
-                res[i] = res[v] + 1
-                q.append(i)
 
-N, M, K, X = map(int, input().split())
-m = [[] for _ in range(N+1)]
-res = [-1] * (N+1)
-for _ in range(M):
-    n1, n2 = map(int, input().split())
-    m[n1].append(n2)
-bfs(X)
-isExist = False
-for i, v in enumerate(res):
-    if v == K:
-        isExist = True
-        print(i)
-if not isExist: print(-1)
+def solve():
+    N, M, K, X = map(int, input().split())
+
+    graph = [[] for _ in range(N + 1)]
+    heap = []
+    answer = []
+    # 인접 리스트 생성
+    for _ in range(M):
+        v1, v2 = map(int, input().split())
+        graph[v1].append(v2)
+
+    # 최소 거리 배열 생성
+    min_dists = [float('inf')] * (N + 1)
+
+    # 최소 힙 세팅
+    min_dists[X] = 0
+    heapq.heappush(heap, [min_dists[X], X])
+
+    # 최소 힙 순회
+    while heap:
+        dist, node = heapq.heappop(heap)
+        if min_dists[node] < dist:
+            continue
+        if min_dists[node] == K:
+            answer.append(str(node))
+            continue
+        for to in graph[node]:
+            if min_dists[to] > dist + 1:
+                min_dists[to] = dist + 1
+                heapq.heappush(heap, [min_dists[to], to])
+
+    return '\n'.join(answer) if answer else -1
+
+
+print(solve())
