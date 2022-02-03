409b35282c3620ca4c5b7f45e129bfe4cfcf6043
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-03 16:55:48 +0900

    [Silver I] Title: 맥주 마시면서 걸어가기, Time: 96 ms, Memory: 32420 KB -BaekjoonHub

--- 백준/Silver/9205.맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Silver/9205.맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,33 +1,25 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
 def bfs():
-    q = deque()
-    q.append([home[0], home[1]])
+    q = deque([[home[0], home[1]]])
+    visited = [0] * n
     while q:
         x, y = q.popleft()
         if abs(x - fest[0]) + abs(y - fest[1]) <= 1000:
             print("happy")
             return
-        for i in range(n):
-            if not visited[i]:
-                new_x, new_y = conv[i]
-                if abs(x - new_x) + abs(y - new_y) <= 1000:
-                    q.append([new_x, new_y])
-                    visited[i] = 1
+        for i, v in enumerate(conv):
+            if not visited[i] and abs(x - v[0]) + abs(y - v[1]) <= 1000:
+                q.append(v)
+                visited[i] = 1
     print("sad")
-    return
-
 
 t = int(input())
-for i in range(t):
+for _ in range(t):
     n = int(input())
-    home = [int(x) for x in input().split()]
-    conv = []
-    for j in range(n):
-        x, y = map(int, input().split())
-        conv.append([x, y])
-    fest = [int(x) for x in input().split()]
-    visited = [ 0 for i in range(n+1)]
+    home = [*map(int, input().split())]
+    conv = [[*map(int, input().split())] for _ in range(n)]
+    fest = [*map(int, input().split())]
     bfs()
