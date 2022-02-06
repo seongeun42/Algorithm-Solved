9f2e181a8bf78c1a74addb0d05887e1842d59257
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-06 22:43:46 +0900

    [Gold V] Title: 보물섬, Time: 860 ms, Memory: 133868 KB -BaekjoonHub

--- 백준/Gold/2589.보물섬/보물섬.py
+++ 백준/Gold/2589.보물섬/보물섬.py
@@ -1,42 +1,18 @@
 from collections import deque
 
-def bfs(x, y):
+def bfs(x, y, visited):
     q = deque([[x, y]])
-    c = []
-    M[y][x] = 1
+    visited[y][x] = 1
     cnt = 0
     while q:
         v = q.popleft()
         for i in range(4):
             a, b = v[0] + dx[i], v[1] + dy[i]
             if 0 <= a < C and 0 <= b < R:
-                if M[b][a] == 'L':
-                    M[b][a] = M[v[1]][v[0]] + 1
-                    cnt = max(cnt, M[b][a])
+                if M[b][a] == 'L' and not visited[b][a]:
+                    visited[b][a] = visited[v[1]][v[0]] + 1
+                    cnt = max(cnt, visited[b][a])
                     q.append([a, b])
-                    c.append([a, b])
-    return find(c, cnt)
-
-def find(land, v):
-    turn = 0
-    cnt = v
-    for l in land:
-        q = deque([[l[0], l[1]]])
-        M[l[1]][l[0]] = -1 if turn % 2 else 1
-        while q:
-            v = q.popleft()
-            for i in range(4):
-                a, b = v[0] + dx[i], v[1] + dy[i]
-                if 0 <= a < C and 0 <= b < R and M[b][a] != 'W':
-                    if turn % 2 and M[b][a] > 0:
-                        M[b][a] = M[v[1]][v[0]] - 1
-                        cnt = max(cnt, abs(M[b][a]))
-                        q.append([a, b])
-                    elif turn % 2 == 0 and M[b][a] < 0:
-                        M[b][a] = M[v[1]][v[0]] + 1
-                        cnt = max(cnt, M[b][a])
-                        q.append([a, b])
-        turn += 1
     return cnt - 1
 
 dx = [1, -1, 0, 0]
@@ -47,5 +23,6 @@ res = 0
 for i in range(R):
     for j in range(C):
         if M[i][j] == 'L':
-            res = max(res, bfs(j, i))
+            visited = [[0] * C for _ in range(R)]
+            res = max(res, bfs(j, i, visited))
 print(res)
