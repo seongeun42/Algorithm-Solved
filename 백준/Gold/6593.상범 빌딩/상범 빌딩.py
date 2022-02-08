6276778096146d3a18559c0f2b853ce39c490832
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-08 21:43:05 +0900

    [Gold V] Title: 상범 빌딩, Time: 164 ms, Memory: 32496 KB -BaekjoonHub

--- 백준/Gold/6593.상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593.상범 빌딩/상범 빌딩.py
@@ -1,47 +1,63 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
+from collections import deque
 
-def bfs(s, e):
-    dx = [1, -1, 0, 0, 0, 0]
-    dy = [0, 0, 1, -1, 0, 0]
-    dz = [0, 0, 0, 0, 1, -1]
-    q = deque([s])
-    B[s[0]][s[1]][s[2]] = 1
+
+def getStart(building, L, R, C):
+    for l in range(L):
+        for r in range(R):
+            for c in range(C):
+                if building[l][r][c] == "S":
+                    return [c, r, l]
+    return -1
+
+
+def solve(building, L, R, C, start):
+    visited = [[[False for _ in range(C)] for _ in range(R)] for _ in range(L)]
+    dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+
+    q = deque()
+    q.append([start[0], start[1], start[2], 0])
+    visited[start[2]][start[1]][start[0]] = True
     while q:
-        v = q.popleft()
-        if v == e:
-            print("Escaped in", B[e[0]][e[1]][e[2]] - 1, "minute(s).")
-            return
-        for i in range(6):
-            x, y, z = v[2] + dx[i], v[1] + dy[i], v[0] + dz[i]
-            if 0 <= x < C and 0 <= y < R and 0 <= z < L:
-                if not B[z][y][x]:
-                    B[z][y][x] = B[v[0]][v[1]][v[2]] + 1
-                    q.append([z, y, x])
-    print("Trapped!")
+        x, y, f, count = q.popleft()
 
-while 1:
+        if building[f][y][x] == "E":
+            return count
+
+        for i in range(4):
+            nxt_x, nxt_y = x + dx[i], y + dy[i]
+            if not (0 <= nxt_x < C and 0 <= nxt_y < R):
+                continue
+            if building[f][nxt_y][nxt_x] == "#":
+                continue
+            if visited[f][nxt_y][nxt_x]:
+                continue
+            visited[f][nxt_y][nxt_x] = True
+            q.append([nxt_x, nxt_y, f, count + 1])
+
+        for v in [-1, 1]:
+            if not (0 <= f + v < L):
+                continue
+            if building[f + v][y][x] == "#":
+                continue
+            if visited[f + v][y][x]:
+                continue
+            visited[f + v][y][x] = True
+            q.append([x, y, f + v, count + 1])
+    return 0
+
+
+input = sys.stdin.readline
+while True:
     L, R, C = map(int, input().split())
-    S, E = [], []
-    if L == R == C == 0:
+    if L == 0 and R == 0 and C == 0:
         break
-    B = []
-    for f in range(L):
-        B.append([])
-        for i in range(R):
-            B[f].append([])
-            a = list(input().rstrip())
-            for j, v in enumerate(a):
-                if v == '.':
-                    B[f][i].append(0)
-                elif v == '#':
-                    B[f][i].append(1)
-                elif v == 'S':
-                    S = [f, i, j]
-                    B[f][i].append(0)
-                else:
-                    E = [f, i, j]
-                    B[f][i].append(0)
+
+    building = []
+    for i in range(L):
+        building.append([input() for _ in range(R)])
         input()
-    bfs(S, E)
+
+    start = getStart(building, L, R, C)
+    elapsed = solve(building, L, R, C, start)
+    print("Trapped!" if elapsed == 0 else f"Escaped in {elapsed} minute(s).")
