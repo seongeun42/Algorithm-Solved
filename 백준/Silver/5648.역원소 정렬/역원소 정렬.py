65a780ffc0113bfd116930d374273b39320a0861
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-13 00:53:14 +0900

    [Silver IV] Title: 역원소 정렬, Time: 84 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Silver/5648.역원소 정렬/역원소 정렬.py
+++ 백준/Silver/5648.역원소 정렬/역원소 정렬.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 n = input().split()
 nums = []
 for v in n[1:]:
-    nums.append(int(''.join(v[::-1])))
+    nums.append(int(v[::-1]))
 n = int(n[0]) - len(nums)
 while n:
     tmp = input().split()
     for v in tmp:
-        nums.append(int(''.join(v[::-1])))
+        nums.append(int(v[::-1]))
     n -= len(tmp)
 print(*sorted(nums), sep='\n')
