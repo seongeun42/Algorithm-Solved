49ff909bba985edaf276811609389e9eec6940d5
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-13 23:56:28 +0900

    [Silver III] Title: N과 M (1), Time: 180 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/15649.N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649.N과 M （1）/N과 M （1）.py
@@ -1,19 +1,5 @@
-def backtrack(dep):
-    if dep == M:
-        print(*res, sep=' ')
-        return
-    for i in range(1, N + 1):
-        f = 0
-        for j in range(dep):
-            if res[j] == i:
-                f = 1
-                break
-        if not f:
-            res[dep] = i
-            backtrack(dep + 1)
-
+from itertools import permutations
 N, M = map(int, input().split())
-res = [0] * M
-for i in range(1, N + 1):
-    res[0] = i
-    backtrack(1)
+res = list(permutations(range(1, N + 1), M))
+for v in res:
+    print(*v, sep=' ')
