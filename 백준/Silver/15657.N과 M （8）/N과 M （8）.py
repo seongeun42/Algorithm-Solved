c99bd99c4854000c3468f7a70af248517621914e
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-15 01:21:46 +0900

    [Silver III] Title: N과 M (8), Time: 80 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15657.N과 M （8）/N과 M （8）.py
@@ -0,0 +1,6 @@
+from itertools import combinations_with_replacement
+N, M = map(int, input().split())
+nums = sorted([*map(int, input().split())])
+res = list(combinations_with_replacement(nums, M))
+for v in res:
+    print(*v, sep=' ')
