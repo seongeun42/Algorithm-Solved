5c2d9177a7e7c75a324a948bf6f19beefb3316e8
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-14 21:16:46 +0900

    [Silver II] Title: 부분수열의 합, Time: 492 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/1182.부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182.부분수열의 합/부분수열의 합.py
@@ -1,9 +1,14 @@
-from itertools import combinations
+def backtrack(dep, total):
+    global cnt
+    if dep == N:
+        return
+    if total + nums[dep] == S:
+        cnt += 1
+    backtrack(dep + 1, total)
+    backtrack(dep + 1, total + nums[dep])
+
 N, S = map(int, input().split())
-nums = [*map(int, input().split())]
+nums = sorted([*map(int, input().split())])
 cnt = 0
-for i in range(1, N + 1):
-    for v in list(combinations(nums, i)):
-        if sum(v) == S:
-            cnt += 1
+backtrack(0, 0)
 print(cnt)
