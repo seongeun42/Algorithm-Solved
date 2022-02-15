e4121856501f21ed3e2491dd78e8d2c71d861131
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-15 22:53:04 +0900

    [Silver II] Title: 로또, Time: 72 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Silver/6603.로또/로또.py
+++ 백준/Silver/6603.로또/로또.py
@@ -1,9 +1,20 @@
-from itertools import combinations
+def backtrack(dep):
+    if dep == 6:
+        print(*res, sep=' ')
+    else:
+        for v in S:
+            if res and v < res[-1]:
+                continue
+            if v not in res:
+                res.append(v)
+                backtrack(dep + 1)
+                res.pop()
+
 while 1:
     ks = [*map(int, input().split())]
+    S = sorted(ks[1:])
     if len(ks) == 1 and not ks[0]:
         break
-    res = list(combinations(ks[1:], 6))
-    for v in res:
-        print(*v, sep=' ')
+    res = []
+    backtrack(0)
     print()
