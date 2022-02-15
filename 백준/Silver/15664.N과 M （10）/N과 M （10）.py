3d69f8c3c5d97956cd78a2b528abf9f9236e862e
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-15 22:40:10 +0900

    [Silver II] Title: N과 M (10), Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/15664.N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664.N과 M （10）/N과 M （10）.py
@@ -1,6 +1,21 @@
-from itertools import combinations
+def backtrack(dep):
+    if dep == M:
+        print(*res, sep=' ')
+    else:
+        cur = -1
+        for i, v in enumerate(nums):
+            if res and v < res[-1]:
+                continue
+            if not used[i] and v != cur:
+                cur = v
+                res.append(v)
+                used[i] = 1
+                backtrack(dep + 1)
+                res.pop()
+                used[i] = 0
+
 N, M = map(int, input().split())
 nums = sorted([*map(int, input().split())])
-res = sorted(set(combinations(nums, M)))
-for v in res:
-    print(*v, sep=' ')
+res = []
+used = [0] * N
+backtrack(0)
