b46f058f83784e6d78bdc38306bc61358424738b
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-15 20:44:12 +0900

    [Silver II] Title: N과 M (9), Time: 136 ms, Memory: 34448 KB -BaekjoonHub

--- 백준/Silver/15663.N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663.N과 M （9）/N과 M （9）.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 N, M = map(int, input().split())
-nums = sorted([*map(int, input().split())])
-res = sorted(list(set(permutations(nums, M))))
+nums = [*map(int, input().split())]
+res = sorted(set(permutations(nums, M)))
 for v in res:
     print(*v, sep=' ')
