562fc50f3a4cfc608d5880d4ca521a9f64a168d6
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-18 14:46:40 +0900

    [Gold IV] Title: 알파벳, Time: 1968 ms, Memory: 49292 KB -BaekjoonHub

--- 백준/Gold/1987.알파벳/알파벳.py
+++ 백준/Gold/1987.알파벳/알파벳.py
@@ -5,20 +5,17 @@ R, C = map(int, input().split())
 M = [list(input().rstrip()) for _ in range(R)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-res = 0
-alp = [0] * 26
 
-def dfs(x, y, cnt):
-    global res
-    res = max(res, cnt)
-    alp[ord(M[y][x]) - 65] = 1
-    for i in range(4):
-        a, b = x + dx[i], y + dy[i]
-        if 0 <= a < C and 0 <= b < R:
-            idx = ord(M[b][a]) - 65
-            if not alp[idx]:
-                dfs(a, b, cnt + 1)
-                alp[idx] = 0
+def bfs():
+    q = set([(0, 0, M[0][0])])
+    cnt = 1
+    while q:
+        x, y, path = q.pop()
+        for i in range(4):
+            a, b = x + dx[i], y + dy[i]
+            if 0 <= a < C and 0 <= b < R and M[b][a] not in path:
+                q.add((a, b, path + M[b][a]))
+                cnt = max(cnt, len(path) + 1)
+    return cnt
 
-dfs(0, 0, 1)
-print(res)
+print(bfs())
