89f4c8b0510b480defc3e718d087bcdf7403674c
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-19 15:12:40 +0900

    [Silver I] Title: 물통, Time: 72 ms, Memory: 30972 KB -BaekjoonHub

--- 백준/Silver/2251.물통/물통.py
+++ 백준/Silver/2251.물통/물통.py
@@ -1,28 +1,24 @@
-from collections import deque
-
-def bfs():
-    q = deque([[0, 0, C]])
-    while q:
-        a, b, c = q.pop()
-        if visited[a][b] == 1: continue
-        if a == 0:
-            res.add(c)
-        visited[a][b] = 1
-        if a + c > A: q.append([A, b, c + a - A])
-        else: q.append([a + c, b, 0])
-        if b + c > B: q.append([a, B, c + b - B])
-        else: q.append([a, b + c, 0])
-        if b + a > B: q.append([a + b - B, B, c])
-        else: q.append([0, a + b, c])
-        if c + a > C: q.append([a + c - C, b, C])
-        else: q.append([0, b, c + a])
-        if a + b > A: q.append([A, b + a - A, c])
-        else: q.append([a + b, 0, c])
-        if c + b > C: q.append([a, b + c - C, C])
-        else: q.append([a, 0, c + b])
-
+def dfs(a, b, c):
+    if visited[a][b] == 1:
+        return
+    visited[a][b] = 1
+    if a == 0:
+        res.append(c)
+    if a + c > A: dfs(A, b, c + a - A)
+    else: dfs(a + c, b, 0)
+    if b + c > B: dfs(a, B, c + b - B)
+    else: dfs(a, b + c, 0)
+    if b + a > B: dfs(a + b - B, B, c)
+    else: dfs(0, a + b, c)
+    if c + a > C: dfs(a + c - C, b, C)
+    else: dfs(0, b, c + a)
+    if a + b > A: dfs(A, b + a - A, c)
+    else: dfs(a + b, 0, c)
+    if c + b > C: dfs(a, b + c - C, C)
+    else: dfs(a, 0, c + b)
+        
 A, B, C = map(int, input().split())
 visited = [[0] * (B + 1) for _ in range(A + 1)]
-res = set()
-bfs()
-print(*sorted(list(res)), sep=' ')
+res = []
+dfs(0, 0, C)
+print(*sorted(res))
