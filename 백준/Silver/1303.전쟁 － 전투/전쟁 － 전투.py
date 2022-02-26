14616f604a0d2dcd0aff8a95f49e695075de3460
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-02-27 04:11:22 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1303.전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,42 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+def dfs(c, r, chr):
+    war[r][c] = 0
+    cnt = 1
+    for i in range(4):
+        x, y = c + dx[i], r + dy[i]
+        if 0 <= x < N and 0 <= y < M and war[y][x] == chr:
+            cnt += dfs(x, y, chr)
+    return cnt
+
+def bfs(c, r, chr):
+    q = deque([[c, r]])
+    war[r][c] = 0
+    cnt = 1
+    while q:
+        v = q.popleft()
+        for i in range(4):
+            x, y = v[0] + dx[i], v[1] + dy[i]
+            if 0 <= x < N and 0 <= y < M and war[y][x] == chr:
+                war[y][x] = 0
+                cnt += 1
+                q.append([x, y])
+    return cnt
+
+N, M = map(int, input().split())
+war = [list(input().strip()) for _ in range(M)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+w, b = 0, 0
+for i in range(M):
+    for j in range(N):
+        if war[i][j] == 'W':
+            v = dfs(j, i, 'W')
+            w += v ** 2
+        elif war[i][j] == 'B':
+            v = dfs(j, i, 'B')
+            b += v ** 2
+print(w, b)
