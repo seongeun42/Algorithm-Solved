ced9281890ad4762c34338189a156529579d2140
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-03-05 18:16:59 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 72 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
@@ -1,17 +1,6 @@
-A, B = map(list, input().split())
-minA, maxA, minB, maxB = '', '', '', ''
-for i, v in enumerate(A):
-    if v == '5' or v == '6':
-        minA += '5'
-        maxA += '6'
-    else:
-        minA += v
-        maxA += v
-for i, v in enumerate(B):
-    if v == '5' or v == '6':
-        minB += '5'
-        maxB += '6'
-    else:
-        minB += v
-        maxB += v
+A, B = input().split()
+minA = A.replace('6', '5')
+maxA = A.replace('5', '6')
+minB = B.replace('6', '5')
+maxB = B.replace('5', '6')
 print(int(minA) + int(minB), int(maxA) + int(maxB))
