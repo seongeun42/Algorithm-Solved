4ff009c4485a3a95829cc8c91c7de897dbcaf827
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-03-16 02:53:20 +0900

    [Silver II] Title: 스타트와 링크, Time: 4540 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,15 +1,26 @@
-from itertools import combinations
+def backtrack(dep, idx):
+    global res
+    if dep == N//2:
+        L = list(set(range(N)) - set(S))
+        start, link = 0, 0
+        for i in range(N//2):
+            for j in range(i+1, N//2):
+                start += abil[S[i]][S[j]] + abil[S[j]][S[i]]
+                link += abil[L[i]][L[j]] + abil[L[j]][L[i]]
+        res = min(res, abs(start - link))
+        return
+
+    for k in range(idx, N):
+        if not p[k]:
+            p[k] = 1
+            S[dep] = k
+            backtrack(dep + 1, k + 1)
+            p[k] = 0
+
 N = int(input())
 abil = [[*map(int, input().split())] for _ in range(N)]
+p = [0] * N
+S = [0] * (N//2)
 res = 1e9
-for S in combinations(range(N), N//2):
-    L = set(range(N)) - set(S)
-    start, link = 0, 0
-    for i in S:
-        for j in S:
-            start += abil[i][j]
-    for i in L:
-        for j in L:
-            link += abil[i][j]
-    res = min(res, abs(start - link))
+backtrack(0, 0)
 print(res)
