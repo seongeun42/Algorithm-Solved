29a1c970b99d3ceea99a269aa9eef09a5474c0ca
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-03-18 03:13:48 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,19 +1,8 @@
-def dfs(idx, dep, s, m, j):
-    if dep == L:
-        if m >= 1 and j >= 2:
-            print(s)
-        return
-    for i in range(idx, C):
-        if not visited[i]:
-            visited[i] = 1
-            if cha[i] in mu:
-                dfs(i + 1, dep + 1, s + cha[i], m + 1, j)
-            else:
-                dfs(i + 1, dep + 1, s + cha[i], m, j + 1)
-            visited[i] = 0
-
+from itertools import combinations as combi
 L, C = map(int, input().split())
 cha = sorted(list(input().split()))
 mu = {'a', 'e', 'i', 'o', 'u'}
-visited = [0] * C
-dfs(0, 0, "", 0, 0)
+for v in combi(cha, L):
+    if list(v) == sorted(v):
+        if len(set(v) & mu) >= 1 and len(set(v) - mu) >= 2:
+            print(''.join(v))
