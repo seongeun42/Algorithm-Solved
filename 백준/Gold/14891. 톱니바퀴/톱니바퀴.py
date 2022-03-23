90dcfe541fa93cf6efc4b2b1c78b08395e690c90
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-03-24 00:38:29 +0900

    [Gold V] Title: 톱니바퀴, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -8,11 +8,8 @@ def rotate(start, r):
 
 def state(s):
     stat = [0] * 3
-    # 톱니바퀴1과 2의 맞닿는 부분 표시
     stat[0] = 0 if t[1][(s[1] + 2) % 8] == t[2][(s[2] + 6) % 8] else 1
-    # 톱니바퀴2과 3의 맞닿는 부분 표시
     stat[1] = 0 if t[2][(s[2] + 2) % 8] == t[3][(s[3] + 6) % 8] else 1
-    # 톱니바퀴3과 4의 맞닿는 부분 표시
     stat[2] = 0 if t[3][(s[3] + 2) % 8] == t[4][(s[4] + 6) % 8] else 1
     return stat
 
@@ -22,7 +19,6 @@ for _ in range(int(input())):
     n, r = map(int, input().split())
     stat = state(s)
     s[n] = rotate(s[n], r)
-    # 오른쪽 톱니바퀴 회전 확인
     turn = r
     for i in range(n + 1, 5):
         turn *= -1
@@ -30,7 +26,6 @@ for _ in range(int(input())):
             break
         else:
             s[i] = rotate(s[i], turn)
-    # 왼쪽 톱니바퀴 회전 확인
     turn = r
     for i in range(n - 1, -1, -1):
         turn *= -1
