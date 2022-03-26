4ba04559a576b04e873ce4f153bdd97d0aeadf96
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-03-27 01:42:37 +0900

    [Silver I] Title: 1로 만들기 2, Time: 160 ms, Memory: 22296 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -4,30 +4,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String dpP[] = new String[n + 1];
+        int dpP[] = new int[n + 1];
         int dpC[] = new int[n + 1];
-        dpC[1] = 0;
-        dpP[1] = "1";
 
         for (int i = 2; i <= n; i++)
         {
             dpC[i] = dpC[i - 1] + 1;
-            dpP[i] = String.valueOf(i) + " " + dpP[i - 1];
+            dpP[i] = i - 1;
 
             if (i % 3 == 0 && dpC[i] > dpC[i / 3])
             {
                 dpC[i] = dpC[i / 3] + 1;
-                dpP[i] = String.valueOf(i) + " " + dpP[i / 3];
+                dpP[i] = i / 3;
             }
 
             if (i % 2 == 0 && dpC[i] > dpC[i / 2])
             {
                 dpC[i] = dpC[i / 2] + 1;
-                dpP[i] = String.valueOf(i) + " " + dpP[i / 2];
+                dpP[i] = i / 2;
             }
         }
 
         System.out.println(dpC[n]);
-        System.out.println(dpP[n]);
+        for (int i = n; i > 0; i = dpP[i])
+            System.out.printf("%d ", i);
     }
 }
