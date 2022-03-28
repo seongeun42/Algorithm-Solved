c071406c674a2ce2caf617c37d8593da234c7d4a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-03-28 21:27:50 +0900

    [Silver V] Title: 3의 배수, Time: 180 ms, Memory: 32816 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,7 +1,7 @@
-X = [*map(int, list(input()))]
+X = input()
 cnt = 0
 while len(X) != 1:
-    X = [*map(int, list(str(sum(X))))]
+    X = str(sum(map(int, X)))
     cnt += 1
 print(cnt)
-print("NO" if X[0] % 3 else "YES")
+print("YES" if X in '369' else "NO")
