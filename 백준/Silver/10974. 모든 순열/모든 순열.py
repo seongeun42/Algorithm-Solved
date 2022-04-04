167dda7cba18774602766860c621436630975d6d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-04-04 22:10:06 +0900

    [Silver III] Title: 모든 순열, Time: 180 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,16 +1,4 @@
-def dfs(dep):
-    if dep == N:
-        print(*arr)
-        return
-    for i in range(N + 1):
-        if not v[i]:
-            v[i] = 1
-            arr.append(i)
-            dfs(dep + 1)
-            arr.pop()
-            v[i] = 0
-
+from itertools import permutations as permu
 N = int(input())
-arr = []
-v = [1] + [0] * N
-dfs(0)
+for v in permu(range(1, N + 1), N):
+    print(*v)
