4cf1622712ed917d606039ca17ae75d447d680de
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-04-05 14:28:17 +0900

    [Silver II] Title: 차이를 최대로, Time: 132 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,26 +1,14 @@
+from itertools import permutations as permu
+
 def cal(arr):
     ans = 0
     for i in range(len(arr) - 1):
-        ans += abs(A[arr[i]] - A[arr[i + 1]])
+        ans += abs(arr[i] - arr[i + 1])
     return ans
-
-def dfs(dep):
-    global res
-    if dep == N:
-        res = max(res, cal(arr))
-        return
-    for i in range(N):
-        if not visit[i]:
-            visit[i] = 1
-            arr.append(i)
-            dfs(dep + 1)
-            arr.pop()
-            visit[i] = 0
 
 N = int(input())
 A = [*map(int, input().split())]
-arr = []
-visit = [0] * N
 res = -1e9
-dfs(0)
+for v in permu(A, N):
+    res = max(res, cal(v))
 print(res)
