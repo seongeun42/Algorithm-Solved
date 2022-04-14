56f9179c685b905d57d11d7b984b623dffc64278
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-04-15 02:54:06 +0900

    [Gold V] Title: 노드사이의 거리, Time: 312 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -2,26 +2,17 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def dfs(node, dist, end):
-    visit[node] = dist
-    for n, w in G[node]:
-        if n == end:
-            visit[end] = dist + w
-            break
-        if not visit[n]:
-            dfs(n, dist + w, end)
-
 def bfs(start, end):
-    q = deque([[start, 1]])
+    q = deque([start])
     visit[start] = 1
     while q:
-        node, dist = q.popleft()
+        node = q.popleft()
+        if node == end:
+            break
         for n, w in G[node]:
-            if n == end:
-                visit[end] = dist + w
-                break
             if not visit[n]:
-                q.append([n, dist + w])
+                visit[n] = visit[node] + w
+                q.append(n)
 
 N, M = map(int, input().split())
 G = [[] for _ in range(N + 1)]
@@ -33,6 +24,5 @@ for _ in range(N - 1):
 for _ in range(M):
     visit = [0] * (N + 1)
     s, e = map(int, input().split())
-    dfs(s, 1, e)
-    # bfs(s, e)
+    bfs(s, e)
     print(visit[e] - 1)
