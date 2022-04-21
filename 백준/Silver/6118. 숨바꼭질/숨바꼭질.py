7fd762914d08783791fb5c4f1bd15e436c9a7cdd
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-04-21 22:45:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 2252 ms, Memory: 37084 KB -BaekjoonHub

---
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs():
+    q = deque([[1, 0]])
+    visit[1] = 1
+    ans = []
+    cur_deg = 0
+    while q:
+        node, deg = q.popleft()
+        if deg == cur_deg:
+            ans.append(node)
+        else:
+            cur_deg = deg
+            ans = [node]
+        for n in G[node]:
+            if not visit[n]:
+                visit[n] = 1
+                q.append([n, deg + 1])
+    print(min(ans), cur_deg, len(ans))
+
+N, M = map(int, input().split())
+G = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    G[a].append(b)
+    G[b].append(a)
+visit = [0] * (N + 1)
+bfs()
