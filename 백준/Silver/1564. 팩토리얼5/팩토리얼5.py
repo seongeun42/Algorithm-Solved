1990140d37d9ba04741e918af4116922ca19009d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-04-27 21:38:52 +0900

    [Silver II] Title: 팩토리얼5, Time: 512 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1564. 팩토리얼5/팩토리얼5.py
+++ 백준/Silver/1564. 팩토리얼5/팩토리얼5.py
@@ -1,7 +1,7 @@
 N = int(input())
-dp = [1, 1]
+res = 1
 for i in range(2, N + 1):
-    dp.append((dp[i - 1] * i) % int(1e13))
-    while dp[i] % 10 == 0:
-        dp[i] //= 10
-print(str(dp[N])[-5:])
+    res = (res * i) % int(1e13)
+    while res % 10 == 0:
+        res //= 10
+print(str(res)[-5:])
