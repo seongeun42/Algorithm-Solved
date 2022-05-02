ae17e64ba7b54ea80a378dc595a227978d5f6db5
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-05-02 14:56:49 +0900

    [Silver I] Title: 현수막, Time: 292 ms, Memory: 99700 KB -BaekjoonHub

---
+++ 백준/Silver/14716. 현수막/현수막.py
@@ -0,0 +1,38 @@
+from _collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(r, c):
+    visited[r][c] = 1
+    for i in range(8):
+        nr, nc = r + dy[i], c + dx[i]
+        if 0 <= nr < M and 0 <= nc < N:
+            if plan[nr][nc] == 1 and not visited[nr][nc]:
+                dfs(nr, nc)
+    return 1
+
+def bfs(r, c):
+    q = deque([[r, c]])
+    visited[r][c] = 1
+    while q:
+        r, c = q.popleft()
+        for i in range(8):
+            nr, nc = r + dy[i], c + dx[i]
+            if 0 <= nr < M and 0 <= nc < N:
+                if plan[nr][nc] == 1 and not visited[nr][nc]:
+                    visited[nr][nc] = 1
+                    q.append([nr, nc])
+    return 1
+
+M, N = map(int, input().split())
+plan = [[*map(int, input().split())] for _ in range(M)]
+visited = [[0] * N for _ in range(M)]
+dx = [1, 1, 1, -1, -1, -1, 0, 0]
+dy = [0, -1, 1, 0, -1, 1, 1, -1]
+cnt = 0
+for i in range(M):
+    for j in range(N):
+        if plan[i][j] == 1 and not visited[i][j]:
+            cnt += dfs(i, j)
+print(cnt)
