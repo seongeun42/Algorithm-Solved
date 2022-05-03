86d670578fab42e16b327ba19572a9aa8fcb266a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-05-03 23:55:59 +0900

    [Silver I] Title: 푸앙이가 길을 건너간 이유, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/20206. 푸앙이가 길을 건너간 이유/푸앙이가 길을 건너간 이유.py
+++ 백준/Silver/20206. 푸앙이가 길을 건너간 이유/푸앙이가 길을 건너간 이유.py
@@ -3,4 +3,4 @@ x1, x2, y1, y2 = map(int, input().split())
 xy = [(x1, y1), (x2, y2)] if B and A / B > 0 else [(x1, y2), (x2, y1)]
 v1 = A * xy[0][0] + B * xy[0][1] + C
 v2 = A * xy[1][0] + B * xy[1][1] + C
-print("Poor" if (v1 < 0 and v2 > 0) or (v1 > 0 and v2 < 0) else "Lucky")
+print("Poor" if v2 and v1 / v2 < 0 else "Lucky")
