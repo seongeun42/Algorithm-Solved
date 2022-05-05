427b3bceb6399f0dd48b43124752f23f2f780f69
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-05-06 00:17:28 +0900

    [Silver I] Title: 흙길 보수하기, Time: 92 ms, Memory: 31860 KB -BaekjoonHub

--- 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.py
@@ -5,11 +5,8 @@ N, L = map(int, input().split())
 W = sorted([[*map(int, input().split())] for _ in range(N)])
 cnt, i = 0, W[0][0]
 for s, e in W:
-    if s < i:
-        s = i
-    V, M = divmod(e - s, L)
+    V, M = divmod(e - max(s, i), L)
     cnt = cnt + V if not M else cnt + V + 1
-    if i < s:
-        i = s
+    i = max(i, s)
     i = i + V * L if not M else i + (V + 1) * L
 print(cnt)
