e85101021cd20ef2e41f17a71538943a823cb79a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-05-12 23:51:58 +0900

    [Gold IV] Title: 택배, Time: 848 ms, Memory: 33932 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,23 +1,32 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
+
+def Dijkstra(s):
+    q = []
+    heapq.heappush(q, [0, s])
+    dp[s][s] = 0
+    while q:
+        s_w, s_n = heapq.heappop(q)
+        if dp[s][s_n] < s_w:
+            continue
+        for w, e_n in G[s_n]:
+            e_w = w + s_w
+            if e_w < dp[s][e_n]:
+                dp[s][e_n] = e_w
+                res[e_n - 1][s - 1] = s_n
+                heapq.heappush(q, [e_w, e_n])
 
 N, M = map(int, input().split())
 res = [['-'] * N for _ in range(N)]
-G = [[1e9] * N for _ in range(N)]
+G = [[] for _ in range(N + 1)]
 for _ in range(M):
     A, B, Time = map(int, input().split())
-    if Time < G[A - 1][B - 1]:
-        G[A - 1][B - 1] = Time
-        G[B - 1][A - 1] = Time
-        res[A - 1][B - 1] = B
-        res[B - 1][A - 1] = A
+    G[A].append([Time, B])
+    G[B].append([Time, A])
+dp = [[1e9] * (N + 1) for _ in range(N + 1)]
 
-for m in range(N):
-    for i in range(N):
-        for j in range(N):
-            if i != j and G[i][j] > G[i][m] + G[m][j]:
-                G[i][j] = G[i][m] + G[m][j]
-                res[i][j] = res[i][m]
+for i in range(1, N + 1):
+    Dijkstra(i)
 
 for v in res:
     print(*v)
