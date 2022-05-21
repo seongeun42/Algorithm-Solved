2d120f55fca0572c16033e9b4f3271a2e0525487
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-05-21 22:11:00 +0900

    [Bronze II] Title: 저항, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,4 @@
+color = {"black": 0, "brown": 1, "red": 2,
+         "orange": 3, "yellow": 4, "green": 5,
+         "blue": 6, "violet": 7, "grey": 8, "white": 9}
+print((color[input()] * 10 + color[input()]) * (10 ** color[input()]))
