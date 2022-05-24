6ec5424743ee4064f200315ebf422dc18efb76ad
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-05-24 12:50:52 +0900

    [Silver II] Title: 스승님 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15979. 스승님 찾기/스승님 찾기.py
+++ 백준/Silver/15979. 스승님 찾기/스승님 찾기.py
@@ -1,6 +1,11 @@
-import math
+def gcd(n1, n2):
+    while n2:
+        n1, n2 = n2, n1 % n2
+    return n1
+
 N, M = map(int, input().split())
 if N == M == 0:
     print(0)
 else:
-    print(1 if math.gcd(N, M) == 1 else 2)
+    v = gcd(N, M)
+    print(1 if v == 1 or v == -1 else 2)
