cb7fb0fc0e705cadeca20e3b9e129b9e8e02904f
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-07-25 01:44:45 +0900

    [Bronze III] Title: TGN, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+for _ in range(N):
+    r, e, c = map(int, input().split())
+    if e - c > r:
+        print("advertise")
+    elif e - c < r:
+        print("do not advertise")
+    else:
+        print("does not matter")
