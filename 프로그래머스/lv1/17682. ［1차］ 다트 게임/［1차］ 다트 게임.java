8c066995b9a5978fbbf32aeadb6e9f31142f579e
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-09-09 04:54:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.13 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(String dartResult) {
+        dartResult = dartResult.replace("10", "a");
         int answer = 0, cnt = -1;
         int[] num = new int[3];
         HashMap<Character, Integer> score = new HashMap<>();
@@ -11,25 +12,17 @@ class Solution {
         
         for (int i = 0; i < dartResult.length(); i++) {
             char c = dartResult.charAt(i);
-            if (Character.isDigit(c)) {
-                cnt++;
-                num[cnt] = c - '0';
-                if (dartResult.charAt(i + 1) == '0') {
-                    num[cnt] = 10;
-                    i++;
-                }
-            }
+            if (Character.isDigit(c) || c == 'a')
+                num[++cnt] = c != 'a' ? c - '0' : 10;
             else if (c == '*') {
                 if (cnt != 0)
                    num[cnt - 1] *= 2;
                 num[cnt] *= 2;
             }
-            else if (c == '#') {
+            else if (c == '#')
                 num[cnt] *= -1;
-            }
-            else {
+            else
                 num[cnt] = (int) Math.pow(num[cnt], score.get(c));
-            }
         }
         
         for (int v : num)
