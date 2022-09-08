921f58067009f00b3ec7db1dfb1b6e38cb68f332
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-09-09 04:48:54 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,16 +1,14 @@
 def solution(dartResult):
     i, cnt = 0, -1
+    dartResult = dartResult.replace("10", "a")
     num = [0, 0, 0]
     score = {'S': 1, 'D': 2, 'T': 3}
     
     while i < len(dartResult):
         c = dartResult[i]
-        if c.isdigit():
+        if c.isdigit() or c == 'a':
             cnt += 1
-            num[cnt] = int(c)
-            if dartResult[i + 1] == '0':
-                num[cnt] = 10
-                i += 1
+            num[cnt] = int(c) if c != 'a' else 10
         elif c == '*':
             if cnt > 0:
                 num[cnt - 1] *= 2
