9ccce3601c3a7597115c180ca00b7b7938d5058a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-09-10 05:21:21 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,28 +1,26 @@
-import java.util.HashSet;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
         // id의 인덱스 만들기
-        HashMap<String, Integer> index = new HashMap<>();
+        Map<String, Integer> index = new HashMap<>();
         for (int i = 0; i < id_list.length; i++)
             index.put(id_list[i], i);
         
         // id 별 신고자 목록 구하기
-        HashMap<Integer, HashSet<String>> user = new HashMap<>();
+        Map<Integer, Set<String>> user = new HashMap<>();
         for (String r : report) {
             String[] rs = r.split(" ");
-            HashSet<String> reporters = user.getOrDefault(index.get(rs[1]), new HashSet<String>());
+            Set<String> reporters = user.getOrDefault(index.get(rs[1]), new HashSet<>());
             reporters.add(rs[0]);
             user.put(index.get(rs[1]), reporters);
         }
         
         // 신고 메일 개수 세기
         for (Integer i : user.keySet()) {
-            HashSet<String> reporters = user.get(i);
+            Set<String> reporters = user.get(i);
             if (reporters.size() >= k) {
                 for (String id : reporters)
                     answer[index.get(id)]++;
