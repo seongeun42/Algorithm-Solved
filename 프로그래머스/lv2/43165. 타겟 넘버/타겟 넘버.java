266f060333161d4557e527c91271c1ea2f5693cf
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-09-12 02:12:49 +0900

    [level 2] Title: 타겟 넘버, Time: 3.19 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, target, 0, 0);
+        return bfs(numbers, target);
     }
     
     public int dfs(int[] numbers, int target, int hap, int dep) {
@@ -11,4 +13,31 @@ class Solution {
         cnt += dfs(numbers, target, hap - numbers[dep], dep + 1);
         return cnt;
     }
+    
+    public int bfs(int[] numbers, int target) {
+        Queue<Node> q = new LinkedList<>();
+        int cnt = 0;
+        q.offer(new Node(0, 0));
+        while (!q.isEmpty()) {
+            Node n = q.poll();
+            if (n.i < numbers.length) {
+                q.offer(new Node(n.i + 1, n.v + numbers[n.i]));
+                q.offer(new Node(n.i + 1, n.v - numbers[n.i]));
+            }
+            else if (n.v == target)
+                cnt++;
+        }
+        return cnt;
+    }
+    
+    class Node {
+        int i;
+        int v;
+        
+        public Node(int i, int v) {
+            this.i = i;
+            this.v = v;
+        }
+    }
 }
+
