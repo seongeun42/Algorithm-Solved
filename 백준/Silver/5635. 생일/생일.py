6527e90e5e42fd30feba6955249805847942138c
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-11-03 22:21:37 +0900

    [Silver V] Title: 생일, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -1,8 +1,5 @@
 n = int(input())
-students = []
-for _ in range(n):
-    name, d, m, y = input().split()
-    students.append([name, int(d), int(m), int(y)])
-students.sort(key=lambda x : (x[3], x[2], x[1]))
-print(students[-1][0])
-print(students[0][0])
+c = [input().split() for _ in range(n)]
+c.sort(key=lambda x : (int(x[3]), int(x[2]), int(x[1])))
+print(c[-1][0])
+print(c[0][0])
