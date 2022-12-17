0f6b778bef73fe2f97e4359a31f2ba1af96e62ea
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-12-17 23:40:47 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,9 +1,3 @@
 def solution(num, total):
-    if total == 0:
-        end = (num - 1) // 2
-        return list(range(-end, end + 1))
-    end = total + 1
-    while 1:
-        if sum(range(end - num, end)) == total:
-            return list(range(end - num, end))
-        end -= 1
+    start = (total * 2 // num - num + 1) // 2
+    return list(range(start, start + num))
