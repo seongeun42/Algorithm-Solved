a66e407bb6aa9f7c3cb52495381f88ff6d597ad3
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-12-17 23:56:10 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,5 +1,4 @@
 def solution(common):
-    cha = common[1] - common[0]
-    if common[-1] - common[0] == cha * (len(common) - 1):
-        return common[-1] + cha
-    return common[-1] * (common[1] // common[0])
+    if common[1] - common[0] == common[2] - common[1]:
+        return common[1] - common[0] + common[-1]
+    return common[1] // common[0] * common[-1]
