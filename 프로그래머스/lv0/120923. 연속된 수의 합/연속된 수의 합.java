e3622bb144df01780074149e423c8f21dd57f453
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-12-17 23:45:00 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int start = (total * 2 / num - num + 1) / 2;
+        for (int i = 0; i < num; i++) {
+            answer[i] = start + i;
+        }
+        return answer;
+    }
+}
