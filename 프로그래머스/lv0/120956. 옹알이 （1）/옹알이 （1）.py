a67258cf0cd182c80e6fde4a179198818b7705a1
Seongeun <64778589+seongeun42@users.noreply.github.com>
2022-12-21 19:48:26 +0900

    [level 0] Title: 옹알이 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,14 +1,9 @@
-def check(word):
-    p = {"a": "aya", "y": "ye", "w": "woo", "m": "ma"}
-    i = 0
-    while i < len(word):
-        if word[i] not in p or word[i:i + len(p[word[i]])] != p[word[i]]:
-            return 0
-        i += len(p[word[i]]) if word[i] in p else 1
-    return 1
-
 def solution(babbling):
     answer = 0
-    for w in babbling:
-        answer += check(w)
+    words = ["aya", "ye", "woo", "ma"]
+    for b in babbling:
+        for w in words:
+            b = b.replace(w, ".")
+        if len(b.replace(".", "")) == 0:
+            answer += 1
     return answer
