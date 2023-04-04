794b8cca5b01a1b9b2567797ceea0f36999cc903
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-04-05 01:21:51 +0900

    [Bronze I] Title: 박스, Time: 336 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+    m, n = map(int, input().split())
+    grid = [[*map(int, input().split())] for _ in range(m)]
+    st = [m] * n
+    ans = 0
+    for i in range(m - 1, -1, -1):
+        for j in range(n):
+            if grid[i][j] == 1:
+                st[j] -= 1
+                ans += st[j] - i
+    print(ans)
