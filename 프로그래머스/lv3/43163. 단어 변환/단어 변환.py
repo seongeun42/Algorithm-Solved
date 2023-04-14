c05709bbb964228523d770c8440f8f97e50fe1c1
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-04-15 00:47:01 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def diff(w1, w2):
+    cnt = 0
+    for i in range(len(w1)):
+        if w1[i] != w2[i]:
+            cnt += 1
+    return cnt
+
+def bfs(begin, target, words):
+    q = deque([(begin, 0)])
+    visit = [0] * len(words)
+    while q:
+        cw, dep = q.popleft()
+        if diff(cw, target) == 1:
+            return dep + 1
+        for i in range(len(words)):
+            if not visit[i] and diff(cw, words[i]) == 1:
+                visit[i] = 1
+                q.append((words[i], dep + 1))
+    return 0
+    
+def solution(begin, target, words):
+    if target not in words: return 0
+    return bfs(begin, target, words)
