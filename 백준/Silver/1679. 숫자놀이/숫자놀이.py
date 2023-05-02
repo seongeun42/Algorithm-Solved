193cd88e1943c16d4139d388c0184205c87e455a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-05-02 16:31:43 +0900

    [Silver I] Title: 숫자놀이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1679. 숫자놀이/숫자놀이.py
+++ 백준/Silver/1679. 숫자놀이/숫자놀이.py
@@ -1,28 +1,22 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def bfs():
-    q = deque([(v, 1) for v in nums])
+def dfs(num, dep):
+    if dep == k:
+        return
     for i in range(n):
-        dp[1][nums[i]] = 1
-    while q:
-        cv, cnt = q.popleft()
-        if cnt == k:
-            continue
-        for i in range(n):
-            nv = cv + nums[i]
-            make.add(nv)
-            if dp[cnt + 1][nv] != 1:
-                dp[cnt + 1][nv] = 1
-                q.append((nv, cnt + 1))
+        nextNum = num + nums[i]
+        make.add(nextNum)
+        if dp[dep + 1][nextNum] != 1:
+            dp[dep + 1][nextNum] = 1
+            dfs(nextNum, dep + 1)
 
 n = int(input())
 nums = [*map(int, input().split())]
 k = int(input())
 make = set(nums)
 dp = [[0] * (nums[-1] * k + 1) for _ in range(k + 1)]
-bfs()
+dfs(0, 0)
 make = sorted(list(make))
 for i in range(len(make)):
     if i + 1 != make[i]:
