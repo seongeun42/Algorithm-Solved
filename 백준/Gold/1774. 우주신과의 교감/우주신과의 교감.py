ebc58e5369a2a3beae5eb77614abc5a84670c1f1
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-05-04 14:24:16 +0900

    [Gold III] Title: 우주신과의 교감, Time: 1368 ms, Memory: 100720 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.py
@@ -1,4 +1,4 @@
-import sys, math
+import sys
 input = sys.stdin.readline
 
 def find_root(node):
@@ -9,7 +9,7 @@ def find_root(node):
 def calDist(n1, n2):
     x1, y1 = node[n1]
     x2, y2 = node[n2]
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 n, m = map(int, input().split())
 node = {(i + 1): tuple(map(int, input().split())) for i in range(n)}
@@ -17,14 +17,12 @@ edge = []
 for i in range(1, n + 1):
     for j in range(i + 1, n + 1):
         edge.append((calDist(i, j), i, j))
+for i in range(m):
+    a, b = map(int, input().split())
+    edge.append((0, a, b))
 edge.sort()
 
 R = [i for i in range(n + 1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    ar = find_root(a)
-    br = find_root(b)
-    R[max(ar, br)] = R[min(ar, br)]
 
 res = 0
 for c, i, j in edge:
