8f0fbbfa07154c4713410abbf1f6eef469ff6f34
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-05-06 21:15:12 +0900

    [Gold III] Title: 파티, Time: 64 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,31 +1,29 @@
 import sys, heapq
 input = sys.stdin.readline
 
-def dijkstra(start):
+def dijkstra(start, G, dp):
     hq = []
     heapq.heappush(hq, (0, start))
+    dp[start] = 0
     while hq:
         s_w, s_n = heapq.heappop(hq)
-        if dp[start][s_n] < s_w:
+        if dp[s_n] < s_w:
             continue
         for w, e_n in G[s_n]:
             e_w = s_w + w
-            if e_w < dp[start][e_n]:
-                dp[start][e_n] = e_w
+            if e_w < dp[e_n]:
+                dp[e_n] = e_w
                 heapq.heappush(hq, (e_w, e_n))
 
 n, m, x = map(int, input().split())
-G = [[] for _ in range(n)]
+G_GO = [[] for _ in range(n)]
+G_BACK = [[] for _ in range(n)]
 for _ in range(m):
     a, b, t = map(int, input().split())
-    G[a - 1].append((t, b - 1))
-dp = [[1e9] * n for _ in range(n)]
-for i in range(n):
-    dp[i][i] = 0
-    dijkstra(i)
-
-cost = [0] * n
-for i in range(n):
-    cost[i] = dp[x - 1][i] + dp[i][x - 1]
-
-print(max(cost))
+    G_GO[a - 1].append((t, b - 1))
+    G_BACK[b - 1].append((t, a - 1))
+go = [1e9] * n
+back = [1e9] * n
+dijkstra(x - 1, G_GO, go)
+dijkstra(x - 1, G_BACK, back)
+print(max([go[i] + back[i] for i in range(n)]))
