2f5c30213c51850427529dfcbb2356db15b19651
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-05-19 19:55:07 +0900

    [Gold III] Title: 거울 설치, Time: 1820 ms, Memory: 95440 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.py
+++ 백준/Gold/2151. 거울 설치/거울 설치.py
@@ -4,28 +4,25 @@ input = sys.stdin.readline
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def dijkstra():
-    hq = []  # (cnt, x, y, direct)
+def dijkstra(sx, sy):
+    hq = []
     for i in range(4):
-        nx, ny = door[0][0] + dx[i], door[0][1] + dy[i]
+        nx, ny = sx + dx[i], sy + dy[i]
         if 0 <= nx < n and 0 <= ny < n and home[ny][nx] != "*":
-            dp[door[0][1]][door[0][0]][i] = 0
-            heapq.heappush(hq, (0, door[0][0], door[0][1], i))
+            dp[sy][sx][i] = 0
+            heapq.heappush(hq, (0, sx, sy, i))
 
     while hq:
         ccnt, cx, cy, cd = heapq.heappop(hq)
         if dp[cy][cx][cd] < ccnt:
             continue
-        if home[cy][cx] in [".", "#"]:
-            nx, ny = cx + dx[cd], cy + dy[cd]
-            if 0 <= nx < n and 0 <= ny < n and home[ny][nx] != "*":
-                if ccnt < dp[ny][nx][cd]:
-                    dp[ny][nx][cd] = ccnt
-                    heapq.heappush(hq, (ccnt, nx, ny, cd))
-        elif home[cy][cx] == "!":
-            for i in range(4):
-                nx, ny = cx + dx[i], cy + dy[i]
-                if 0 <= nx < n and 0 <= ny < n and home[ny][nx] != "*":
+        nx, ny = cx + dx[cd], cy + dy[cd]
+        if 0 <= nx < n and 0 <= ny < n and home[ny][nx] != "*":
+            if home[ny][nx] in [".", "#"] and ccnt < dp[ny][nx][cd]:
+                dp[ny][nx][cd] = ccnt
+                heapq.heappush(hq, (ccnt, nx, ny, cd))
+            elif home[ny][nx] == "!":
+                for i in range(4):
                     w = 1 if i != cd else 0
                     if ccnt + w < dp[ny][nx][i]:
                         dp[ny][nx][i] = ccnt + w
@@ -39,5 +36,5 @@ for i in range(n):
         if home[i][j] == "#":
             door.append((j, i))
 dp = [[[1e9] * 4 for _ in range(n)] for _ in range(n)]
-dijkstra()
+dijkstra(door[0][0], door[0][1])
 print(min(dp[door[1][1]][door[1][0]]))
