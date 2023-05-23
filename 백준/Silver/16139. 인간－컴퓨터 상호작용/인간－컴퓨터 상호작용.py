70ec82181f6dc606714a3b0232aa720aafec884d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-05-23 17:37:32 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+q = int(input())
+dp = [[0] * 28 for _ in range(len(s))]
+a = ord('a')
+dp[0][ord(s[0]) - a] = 1
+for i in range(1, len(s)):
+    for j in range(28):
+        dp[i][j] = dp[i - 1][j]
+    dp[i][ord(s[i]) - a] += 1
+for _ in range(q):
+    c, l, r = input().split()
+    l, r = int(l), int(r)
+    cnt = dp[r][ord(c) - a]
+    if l != 0:
+        cnt -= dp[l - 1][ord(c) - a]
+    print(cnt)
