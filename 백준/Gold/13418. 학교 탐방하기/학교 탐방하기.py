0b651221707392b6da40e0afb5445e762571d00d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-05-25 20:33:49 +0900

    [Gold III] Title: 학교 탐방하기, Time: 2872 ms, Memory: 166796 KB -BaekjoonHub

---
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def find_root(R, n):
+    if R[n] != n:
+        R[n] = find_root(R, R[n])
+    return R[n]
+
+def cal(R, P):
+    val = 0
+    for a, b, c in P:
+        ar = find_root(R, a)
+        br = find_root(R, b)
+        if ar != br:
+            val += not c
+            if ar < br:
+                R[br] = ar
+            else:
+                R[ar] = br
+    return val ** 2
+
+n, m = map(int, input().split())
+path = [[*map(int, input().split())] for _ in range(m + 1)]
+worstR = [i for i in range(n + 1)]
+bestR = [i for i in range(n + 1)]
+best = cal(bestR, sorted(path, key=lambda x: (-x[2], x[0], x[1])))
+worst = cal(worstR, sorted(path, key=lambda x: (x[2], x[0], x[1])))
+print(worst - best)
