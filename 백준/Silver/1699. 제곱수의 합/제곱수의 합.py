2a14ee4617cf28f6c252cf3564dda8a66e55295b
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-07-22 02:21:53 +0900

    [Silver II] Title: 제곱수의 합, Time: 3860 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,16 +1,15 @@
 n = int(input())
-
 sqrt = [0, 1]
 dp = [0, 1, 2, 3]
 for i in range(4, n + 1):
-    dp.append(100000)
+    dp.append(n)
     if (i ** 0.5) % 1 == 0:
         dp[i] = 1
         sqrt.append(i)
-    else:
-        for j in range(len(sqrt) - 1, 0, -1):
-            if dp[i] == 2:
-                break
-            dp[i] = min(dp[i], dp[i - sqrt[j]] + dp[sqrt[j]])
-
+        continue
+    for j in range(len(sqrt) - 1, 0, -1):
+        if dp[i] == 2:
+            break
+        if dp[i] > dp[i - sqrt[j]] + dp[sqrt[j]]:
+            dp[i] = dp[i - sqrt[j]] + dp[sqrt[j]]
 print(dp[n])
