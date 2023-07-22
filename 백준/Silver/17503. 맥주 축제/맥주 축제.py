c2a8d4a7a1c99a4d917584776a1be32256333657
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-07-23 01:48:40 +0900

    [Silver I] Title: 맥주 축제, Time: 688 ms, Memory: 78272 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.py
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -1,26 +1,18 @@
-import sys
-import heapq
-day, sum_taste, kind = map(int,input().split())
-l = []
-pq = []
-heapq.heapify(pq)
-for _ in range(kind):
-    taste, alch = map(int,sys.stdin.readline().split())
-    l.append((taste,alch))
-l.sort(key=lambda x: (x[1],x[0]))
-find = False
-now_alchol = 0
-s = 0
-for i in range(kind):
-    heapq.heappush(pq,l[i][0])
-    s += l[i][0]
-    now_alchol = l[i][1]
-    if len(pq) == day:
-        if s >= sum_taste:
-            find = True
-            print(now_alchol)
+import sys, heapq
+input = sys.stdin.readline
+N, M, K = map(int, input().split())
+beer = sorted([[*map(int, input().split())] for _ in range(K)], key=lambda x: x[1])
+drink = []
+prefer = 0
+find = 0
+for i in range(K):
+    heapq.heappush(drink, beer[i][0])
+    prefer += beer[i][0]
+    if len(drink) == N:
+        if prefer >= M:
+            find = 1
+            print(beer[i][1])
             break
         else:
-            s -= heapq.heappop(pq)
-if not find:
-    print(-1)
+            prefer -= heapq.heappop(drink)
+if not find: print(-1)
