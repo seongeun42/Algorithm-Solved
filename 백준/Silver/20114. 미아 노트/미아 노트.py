6795df7d2d001c8f2555b2b9de20a966505c8c53
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-07-30 18:25:41 +0900

    [Silver V] Title: 미아 노트, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/20114. 미아 노트/미아 노트.py
+++ 백준/Silver/20114. 미아 노트/미아 노트.py
@@ -1,15 +1,8 @@
 N, H, W = map(int, input().split())
-strs = [list(input()) for _ in range(H)]
-ans = ""
-for i in range(1, H):
-    for j in range(N * W):
-        if strs[0][j] != "?": continue
-        if strs[i][j] != "?":
-            strs[0][j] = strs[i][j]
-for i in range(0, N * W, W):
-    c = "?"
-    for j in range(i, i + W):
-        if strs[0][j] != "?":
-            c = strs[0][j]
-    ans += c
-print(ans)
+ans = ['?'] * N
+for _ in range(H):
+    s = input()
+    for i in range(N * W):
+        if s[i] != '?':
+            ans[i // W] = s[i]
+print("".join(ans))
