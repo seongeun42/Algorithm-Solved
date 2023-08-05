6a3e00ead39c00d755a893580b81e7d4df3ac52b
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-06 00:21:25 +0900

    [Silver I] Title: 부동산 다툼, Time: 636 ms, Memory: 48668 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.py
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.py
@@ -1,23 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(pre, idx):
+def dfs(pre, idx, live):
     if pre == 1:
         return idx
     parent = pre // 2
     if parent in live:
-        idx = dfs(parent, parent)
+        idx = dfs(parent, parent, live)
     else:
-        idx = dfs(parent, idx)
+        idx = dfs(parent, idx, live)
     return idx
 
-n, q = map(int, input().split())
-live = set()
-live.add(int(input()))
-print(0)
-for _ in range(q - 1):
-    x = int(input())
-    res = dfs(x, 0 if x not in live else x)
-    if res == 0:
-        live.add(x)
-    print(res)
+def solve(q):
+    live = {int(input())}
+    print(0)
+    for _ in range(q - 1):
+        x = int(input())
+        res = dfs(x, 0 if x not in live else x, live)
+        if res == 0:
+            live.add(x)
+        print(res)
+
+if __name__ == "__main__" :
+    n, q = map(int, input().split())
+    solve(q)
