a5011db5138ecf2d4730db8d0257001a7330c025
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-05 20:19:00 +0900

    [Gold V] Title: 신기한 소수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,22 +1,18 @@
 def isPrime(num):
-    if num in sosu:
-        return True
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
-    sosu.add(num)
     return True
 
 def dfs(dep, num):
     if dep == N:
         print(num)
         return
-    for i in range(10):
+    for i in range(1, 10, 2):
         tmp = num * 10 + i
         if isPrime(tmp):
             dfs(dep + 1, tmp)
 
 N = int(input())
-sosu = {2, 3, 5, 7}
 for i in [2, 3, 5, 7]:
     dfs(1, i)
