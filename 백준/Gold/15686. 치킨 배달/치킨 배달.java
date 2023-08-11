2b9c800d7dd376e025bf11396f451c2ab511953c
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-11 15:44:13 +0900

    [Gold V] Title: 치킨 배달, Time: 196 ms, Memory: 15500 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -23,18 +23,17 @@ public class Main {
             }
         }
 
-        boolean[] open = new boolean[chik.size()];
-        solve(0, 0, n, m, home, chik, open);
-        System.out.println(res);
-    }
-   
-    public static void solve(int idx, int dep, int n, int m, List<Integer[]> home, List<Integer[]> chik, boolean[] open) {
-        if (dep == m) {
+        int[] open = new int[chik.size()];
+        for (int i = chik.size() - 1; i >= chik.size() - m; i--) {
+            open[i] = 1;
+        }
+
+        do {
             int total = 0;
             for (Integer[] h : home) {
                 int min_v = Integer.MAX_VALUE;
                 for (int i = 0; i < chik.size(); i++) {
-                    if (open[i]) {
+                    if (open[i] == 1) {
                         Integer[] c = chik.get(i);
                         min_v = Math.min(min_v, Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]));
                     }
@@ -44,12 +43,27 @@ public class Main {
                     break;
             }
             res = Math.min(res, total);
-        }
-
-        for (int i = idx; i < chik.size(); i++) {
-            open[i] = true;
-            solve(i + 1, dep + 1, n, m, home, chik, open);
-            open[i] = false;
-        }
+        } while (np(open));
+        System.out.println(res);
     }
+
+    static boolean np(int[] mask) {
+        int N = mask.length;
+        int i = N - 1;
+        while (i > 0 && mask[i - 1] >= mask[i]) i--;
+
+        if (i == 0) return false;
+
+        int j = N - 1;
+        while (mask[i - 1] >= mask[j]) j--;
+
+        int temp = mask[i - 1];
+        mask[i - 1] = mask[j];
+        mask[j] = temp;
+
+        Arrays.sort(mask, i, mask.length);
+
+        return true;
+    }
+
 }
