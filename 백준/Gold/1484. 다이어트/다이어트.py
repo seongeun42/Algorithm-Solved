f521d12feed31f01adc4b13083e6eaa48cca4e60
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-12 20:24:09 +0900

    [Gold V] Title: 다이어트, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.py
+++ 백준/Gold/1484. 다이어트/다이어트.py
@@ -3,18 +3,18 @@ def solve():
     ans = []
     s, e = 1, 2
     while 1:
-        if (s + 1) ** 2 - s ** 2 > G:
+        if s * 2 >= G:
             break
-        v = e ** 2 - s ** 2
+        v = (e + s) * (e - s)
         if v == G:
             ans.append(e)
         elif v < G:
             e += 1
             continue
         s += 1
-    if len(ans) == 0:
-        print(-1)
-    else:
+    if len(ans):
         print(*ans, sep="\n")
+    else:
+        print(-1)
 
 solve()
