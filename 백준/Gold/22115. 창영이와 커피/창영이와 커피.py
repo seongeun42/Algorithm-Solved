97abc92afa50c289f467d18d2763c36d914bc338
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-13 03:31:27 +0900

    [Gold V] Title: 창영이와 커피, Time: 228 ms, Memory: 32820 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.py
@@ -1,11 +1,13 @@
 def solve():
-    N, K = map(int, input().split())
+    _, K = map(int, input().split())
     C = [*map(int, input().split())]
     dp = [0] + [101] * K
+    maxC = 0
     for c in C:
-        for k in range(K, c - 1, -1):
-            if dp[k] > dp[k - c]:
+        for k in range(min(K, maxC + c), c - 1, -1):
+            if dp[k] > dp[k - c] + 1:
                 dp[k] = dp[k - c] + 1
+        maxC += c
     print(-1 if dp[K] == 101 else dp[K])
 
 solve()
