32e567fc59a3be3d7c40e7054e6eddc5c0a97131
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-13 04:45:42 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 104 ms, Memory: 31724 KB -BaekjoonHub

---
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.py
@@ -0,0 +1,15 @@
+import sys, copy
+input = sys.stdin.readline
+
+def solve():
+    N, M, H = map(int, input().split())
+    dp = [1] + [0] * H
+    for _ in range(N):
+        B = [*map(int, input().split())]
+        tmp = copy.deepcopy(dp)
+        for b in B:
+            for h in range(H, b - 1, -1):
+                dp[h] += tmp[h - b]
+    print(dp[H] % 10007)
+
+solve()
