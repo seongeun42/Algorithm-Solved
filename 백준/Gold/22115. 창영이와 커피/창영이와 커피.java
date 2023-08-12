eadb7b4d2c479c442e2e109c95c139b13d395935
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-13 03:39:02 +0900

    [Gold V] Title: 창영이와 커피, Time: 112 ms, Memory: 12488 KB -BaekjoonHub

---
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K + 1];
+        Arrays.fill(dp, 101);
+        dp[0] = 0;
+        st = new StringTokenizer(br.readLine());
+        int all = 0;
+        for (int i = 0; i < N; i++) {
+            int c = Integer.parseInt(st.nextToken());
+            for (int k = Math.min(all + c, K); k >= c; k--) {
+                if (dp[k] > dp[k - c] + 1)
+                    dp[k] = dp[k - c] + 1;
+            }
+            all += c;
+        }
+        System.out.println(dp[K] == 101 ? -1 : dp[K]);
+    }
+
+}
