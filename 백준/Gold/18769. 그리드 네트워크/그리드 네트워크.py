af61de4edc30210d4a4d8057b84d079d909882aa
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-08-20 17:22:23 +0900

    [Gold IV] Title: 그리드 네트워크, Time: 44 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.py
+++ 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.py
@@ -1,35 +1,39 @@
 import sys, heapq
 input = sys.stdin.readline
 
-def find_root(node, R):
-    if R[node] != node:
-        R[node] = find_root(R[node], R)
-    return R[node]
+def prim(G, R, C):
+    hq = G[0]
+    heapq.heapify(hq)
+    visited = [0] * (R * C)
+    visited[0] = 1
+    res = 0
+    while hq:
+        w, e = heapq.heappop(hq)
+        if not visited[e]:
+            visited[e] = 1
+            res += w
+            for nw, ne in G[e]:
+                if not visited[ne]:
+                    heapq.heappush(hq, (nw, ne))
+    return res
 
 def solve():
     T = int(input())
     for _ in range(T):
         R, C = map(int, input().split())
-        E = []
+        G = [[] for _ in range(R * C)]
         for i in range(R):
             c = [*map(int, input().split())]
             for j in range(C - 1):
                 n = i * C + j
-                heapq.heappush(E, (c[j], n, n + 1))
+                G[n].append((c[j], n + 1))
+                G[n + 1].append((c[j], n))
         for i in range(R - 1):
             c = [*map(int, input().split())]
             for j in range(C):
                 n = i * C + j
-                heapq.heappush(E, (c[j], n, n + C))
-        R = [i for i in range(R * C)]
-        ans = 0
-        while E:
-            w, a, b = heapq.heappop(E)
-            ar = find_root(a, R)
-            br = find_root(b, R)
-            if ar != br:
-                ans += w
-                R[max(ar, br)] = min(ar, br)
-        print(ans)
+                G[n].append((c[j], n + C))
+                G[n + C].append((c[j], n))
+        print(prim(G, R, C))
 
 solve()
