9e3b46e704be4910eaf81a28f8c0f8ad457ea053
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-09-08 20:23:17 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    T = int(input())
+    dp = [1] * 10001
+    for i in range(2, 10001):
+        dp[i] += dp[i - 2]
+    for i in range(3, 10001):
+        dp[i] += dp[i - 3]
+    for _ in range(T):
+        print(dp[int(input())])
