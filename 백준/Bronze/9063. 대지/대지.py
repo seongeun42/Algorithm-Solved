fab65ce2d765d680189576df6fad324ea3227f00
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-11-12 22:10:27 +0900

    [Bronze III] Title: 대지, Time: 2096 ms, Memory: 35060 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,7 @@
+N = int(input())
+xlist, ylist = [], []
+for _ in range(N):
+    x, y = map(int, input().split())
+    xlist.append(x)
+    ylist.append(y)
+print((max(xlist) - min(xlist)) * (max(ylist) - min(ylist)))
