affe162e5917646c9349a68bf0de2a52cd9b0067
Seongeun <64778589+seongeun42@users.noreply.github.com>
2023-12-16 23:03:24 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 4616 ms, Memory: 122384 KB -BaekjoonHub

--- 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.py
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.py
@@ -1,21 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-while True:
-    try:
-        x = int(input()) * 10000000
-        n = int(input())
-        lego = sorted([int(input()) for _ in range(n)])
-        s, e = 0, n - 1
-        ans = "danger"
-        while s < e:
-            if lego[s] + lego[e] == x:
-                ans = f"yes {lego[s]} {lego[e]}"
-                break
-            if lego[s] + lego[e] > x:
-                e -= 1
-            else:
-                s += 1
-        print(ans)
-    except Exception:
-        break
+def solve():
+    while True:
+        try:
+            x = int(input()) * 10000000
+            n = int(input())
+            lego = sorted([int(input()) for _ in range(n)])
+            if n < 2:
+                print("danger")
+                continue
+            s, e = 0, n - 1
+            ans = "danger"
+            while s < e:
+                if lego[s] + lego[e] == x:
+                    ans = f"yes {lego[s]} {lego[e]}"
+                    break
+                if lego[s] + lego[e] > x:
+                    e -= 1
+                else:
+                    s += 1
+            print(ans)
+        except Exception:
+            break
+
+solve()
