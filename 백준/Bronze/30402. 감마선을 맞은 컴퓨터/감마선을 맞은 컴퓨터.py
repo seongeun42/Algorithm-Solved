3ef846e2f0523ea375c40258c1eb350e68ade9a7
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-01-13 23:09:15 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for _ in range(15):
+    b = list(map(str, input().split()))
+    if 'w' in b:
+        print("chunbae")
+        break
+    elif 'b' in b:
+        print("nabi")
+        break
+    elif 'g' in b:
+        print("yeongcheol")
+        break
