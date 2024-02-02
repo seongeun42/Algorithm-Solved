a898e9029a76a511624a18dbbc88a2c44d951939
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-02 22:15:48 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.read
+
+s = input().replace("\n","").replace(" ","")
+c = [0] * 26
+for i in s:
+    c[ord(i)-97] += 1
+maxx = max(c)
+r = []
+for i in range(len(c)):
+    if c[i] == maxx:
+        r.append(chr(i + 97))        
+print(*sorted(r), sep="")
