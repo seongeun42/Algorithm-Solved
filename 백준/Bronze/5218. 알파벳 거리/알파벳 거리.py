cda2b12156865ec87dea323c107221b91b77233b
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-02 22:39:46 +0900

    [Bronze II] Title: 알파벳 거리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5218. 알파벳 거리/알파벳 거리.py
@@ -0,0 +1,10 @@
+n = int(input())
+for _ in range(n):
+	s = []
+	N, M = map(list, input().split())
+	for i in range(len(N)):
+		if ord(N[i]) <= ord(M[i]):
+			s.append(ord(M[i]) - ord(N[i]))
+		else:
+			s.append(ord(M[i]) + 26 - ord(N[i]))
+	print(f'Distances: {" ".join(map(str, s))}')
