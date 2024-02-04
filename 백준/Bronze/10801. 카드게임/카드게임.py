ad6a9d6f9c5926e3047b76746f10a9bf4b3ca7e9
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-04 20:02:37 +0900

    [Bronze II] Title: 카드게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10801. 카드게임/카드게임.py
@@ -0,0 +1,14 @@
+A = [*map(int, input().split())]
+B = [*map(int, input().split())]
+A_cnt, B_cnt = 0, 0
+for A_card, B_card in zip(A, B):
+    if A_card > B_card:
+        A_cnt += 1
+    elif A_card < B_card:
+        B_cnt += 1
+if A_cnt > B_cnt:
+    print('A')
+elif A_cnt < B_cnt:
+    print('B')
+else:
+    print('D')
