d62ca295d555b96ca0670678b316d90ba33cb704
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-22 21:11:14 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 212 ms, Memory: 57784 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -1,31 +1,19 @@
 import sys, math
 input = sys.stdin.readline
 
-def simulate(room, mHP, cHP, atk):
-    for t, a, h in room:
-        if t == 1:
-            cnt = int(math.ceil(h / atk))
-            if cHP <= a * (cnt - 1):
-                return False
-            cHP -= a * (cnt - 1)
-        else:
-            cHP = min(cHP + h, mHP)
-            atk += a
-    return True
-
 def solve():
     N, atk = map(int, input().split())
     room = [[*map(int, input().split())] for _ in range(N)]
-    s, e = 1, int(9e18)
-    ans = 0
-    while s <= e:
-        mHP = (s + e) // 2
-        alive = simulate(room, mHP, mHP, atk)
-        if not alive:
-            s = mHP + 1
+    mHP = cHP = 0
+    for t, a, h in room:
+        if t == 1:
+            damage = a * (int(math.ceil(h / atk)) - 1)
+            cHP += damage
+            if cHP > mHP:
+                mHP = cHP
         else:
-            ans = mHP
-            e = mHP - 1
-    print(ans)
+            cHP = max(cHP - h, 0)
+            atk += a
+    print(mHP + 1)
 
 solve()
