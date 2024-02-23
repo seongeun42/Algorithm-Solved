bd855617d916fbd73e5ba3d7bf30c34f5a8525b8
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-23 19:30:30 +0900

    [Gold IV] Title: 여왕벌, Time: 1848 ms, Memory: 34448 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -3,34 +3,19 @@ input = sys.stdin.readline
 
 M, N = map(int, input().split())
 larva = [[1] * M for _ in range(M)]
-grow = [[0] * M for _ in range(M)]
+first = [0] * (2 * M - 1)
 for _ in range(N):
     zero, one, two = map(int, input().split())
-    idx = zero
-    while one > 0 and 0 <= idx < M:
-        larva[M - idx - 1][0] += 1
-        grow[M - idx - 1][0] = 1
-        one -= 1
-        idx += 1
-    while one > 0 and M <= idx < 2 * M:
-        larva[0][idx - M + 1] += 1
-        grow[0][idx - M + 1] = 1
-        one -= 1
-        idx += 1
-    while one == 0 and two > 0 and 0 <= idx < M:
-        larva[M - idx - 1][0] += 2
-        grow[M - idx - 1][0] = 2
-        two -= 1
-        idx += 1
-    while one == 0 and two > 0 and M <= idx < 2 * M:
-        larva[0][idx - M + 1] += 2
-        grow[0][idx - M + 1] = 2
-        two -= 1
-        idx += 1
-    for i in range(1, M):
-        for j in range(1, M):
-            mg = max(grow[i - 1][j], grow[i - 1][j - 1], grow[i][j - 1])
-            larva[i][j] += mg
-            grow[i][j] = mg
+    for i in range(one):
+        first[zero + i] += 1
+    for i in range(two):
+        first[zero + one + i] += 2
+for i in range(len(first)):
+    y = 0 if i >= M else M - i - 1
+    x = i - M + 1 if i >= M else 0
+    larva[y][x] += first[i]
+for i in range(1, M):
+    for j in range(1, M):
+        larva[i][j] = max(larva[i - 1][j], larva[i - 1][j - 1], larva[i][j - 1])
 for i in range(M):
     print(*larva[i])
