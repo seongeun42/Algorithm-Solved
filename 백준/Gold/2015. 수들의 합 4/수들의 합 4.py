9014fba374edf46e07a9e3cc16c182f800af72d4
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-25 23:31:36 +0900

    [Gold IV] Title: 수들의 합 4, Time: 96 ms, Memory: 45572 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -1,17 +1,12 @@
-N, K = map(int, input().split())
-A = [*map(int, input().split())]
-hap = [A[0]]
-ans = 0 if A[0] != K else 1
-nums = {A[0]: 1}
-for i in range(1, N):
-    hap.append(hap[i - 1] + A[i])
-    if hap[i] == K:
-        ans += 1
-    target = hap[i] - K
-    if target in nums:
-        ans += nums[target]
-    if hap[i] in nums:
-        nums[hap[i]] += 1
-    else:
-        nums[hap[i]] = 1
-print(ans)
+def solve():
+    N, K = map(int, input().split())
+    A = [*map(int, input().split())]
+    total = ans = 0
+    nums = {0 : 1}
+    for i in range(N):
+        total += A[i]
+        ans += nums.get(total - K, 0)
+        nums[total] = nums.get(total, 0) + 1
+    print(ans)
+
+solve()
