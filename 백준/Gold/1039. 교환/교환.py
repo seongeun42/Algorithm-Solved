e6b4133561a88451030113e9dfb430874b00aa2d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-02-29 20:23:10 +0900

    [Gold II] Title: 교환, Time: 92 ms, Memory: 41260 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.py
+++ 백준/Gold/1039. 교환/교환.py
@@ -1,10 +1,11 @@
 from collections import deque
 
 def bfs(N, K, size):
-    q = {(N, 0)}
+    q = deque([(N, 0)])
+    visited = [-1] * 1000001
     ans = -1
     while q:
-        num, cnt = q.pop()
+        num, cnt = q.popleft()
         if cnt == K:
             if ans < num:
                 ans = num
@@ -19,11 +20,16 @@ def bfs(N, K, size):
                         continue
                     newNum = num - (jnum * (10 ** (jDigit - 1))) - (inum * (10 ** (iDigit - 1)))
                     newNum += (inum * (10 ** (jDigit - 1))) + (jnum * (10 ** (iDigit - 1)))
-                    q.add((newNum, cnt + 1))
+                    if visited[newNum] >= cnt:
+                        continue
+                    visited[newNum] = cnt
+                    q.append((newNum, cnt + 1))
     return ans
     
 def solve():
     N, K = input().split()
-    print(bfs(int(N), int(K), len(N)))
+    if len(N) == 1 or (len(N) == 2 and N[1] == "0"):
+        return -1
+    return bfs(int(N), int(K), len(N))
 
-solve()
+print(solve())
