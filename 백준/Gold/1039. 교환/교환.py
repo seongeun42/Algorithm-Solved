3863a1a6b02fadc4a2e7d12dcf535e18777151b9
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-03-01 15:17:59 +0900

    [Gold II] Title: 교환, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.py
+++ 백준/Gold/1039. 교환/교환.py
@@ -1,35 +1,31 @@
-from collections import deque
+ans = -1
 
-def bfs(N, K, size):
-    q = deque([(N, 0)])
-    visited = [-1] * 1000001
-    ans = -1
-    while q:
-        num, cnt = q.popleft()
-        if cnt == K:
-            if ans < num:
-                ans = num
-        else:
-            for i in range(size):
-                iDigit = size - i
-                inum = num % (10 ** iDigit) // (10 ** (iDigit - 1))
-                for j in range(i + 1, size):
-                    jDigit = size - j
-                    jnum = num % (10 ** jDigit) // (10 ** (jDigit - 1))
-                    if i == 0 and jnum == 0:
-                        continue
-                    newNum = num - (jnum * (10 ** (jDigit - 1))) - (inum * (10 ** (iDigit - 1)))
-                    newNum += (inum * (10 ** (jDigit - 1))) + (jnum * (10 ** (iDigit - 1)))
-                    if visited[newNum] >= cnt:
-                        continue
-                    visited[newNum] = cnt
-                    q.append((newNum, cnt + 1))
-    return ans
+def backtrack(dep, N, K, size, visited):
+    num = int("".join(N))
+    if num in visited[dep]:
+        return
+    visited[dep].add(num)
+    global ans
+    if dep == K:
+        res = num
+        if ans < res:
+            ans = res
+        return
+    for i in range(size):
+        for j in range(i + 1, size):
+            if i == 0 and N[j] == "0":
+                continue
+            N[i], N[j] = N[j], N[i]
+            backtrack(dep + 1, N, K, size, visited)
+            N[i], N[j] = N[j], N[i]
     
 def solve():
     N, K = input().split()
     if len(N) == 1 or (len(N) == 2 and N[1] == "0"):
-        return -1
-    return bfs(int(N), int(K), len(N))
+        print(-1)
+    else:
+        visited = [set() for _ in range(int(K) + 1)]
+        backtrack(0, list(N), int(K), len(N), visited)
+        print(ans)
 
-print(solve())
+solve()
