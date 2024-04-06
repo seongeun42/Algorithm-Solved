8a62eeb428916a59229b5c281f3ba95f08d2f596
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-04-06 12:07:08 +0900

    [Silver I] Title: 관중석, Time: 828 ms, Memory: 63920 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,13 +1,9 @@
 from math import gcd
-
 a, b = map(int, input().split())
-arr = [[0] * b for _ in range(b)]
+arr = [[0] * (b + 1) for _ in range(b + 1)]
 ans = 0
 for i in range(a, b + 1):
     for j in range(1, i + 1):
         g = gcd(i, j)
-        x, y = i // g, j // g
-        if not arr[x - 1][y - 1]:
-            arr[x - 1][y - 1] = 1
-            ans += 1
-print(ans)
+        arr[j // g][i // g] = 1
+print(sum(map(sum, arr)))
