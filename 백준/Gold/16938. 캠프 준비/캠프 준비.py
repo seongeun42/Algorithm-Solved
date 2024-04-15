7cad5b149335775cb01d2e61a30028645a3742f6
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-04-16 08:30:55 +0900

    [Gold V] Title: 캠프 준비, Time: 72 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.py
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.py
@@ -1,16 +1,12 @@
+from itertools import combinations as cb
 import sys
 input = sys.stdin.readline
 
-def backtrack(idx, total, min_v, max_v, dep, max_dep):
-    if total > R:
-        return 0
-    if dep == max_dep:
-        return 1 if L <= total <= R and max_v - min_v >= X else 0
-    cnt = 0
-    for i in range(idx, N):
-        cnt += backtrack(i + 1, total + A[i], min(min_v, A[i]), max(max_v, A[i]), dep + 1, max_dep)
-    return cnt
-
 N, L, R, X = map(int, input().split())
 A = sorted([*map(int, input().split())])
-print(sum([backtrack(0, 0, 1e7, 0, 0, i) for i in range(2, N + 1)]))
+ans = 0
+for i in range(2, N + 1):
+    for list in cb(A, i):
+        if L <= sum(list) <= R and max(list) - min(list) >= X:
+            ans += 1
+print(ans)
