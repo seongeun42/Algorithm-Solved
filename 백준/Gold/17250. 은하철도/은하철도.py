2c9a44d331ce7a878dd68e0991970dd422c474ad
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-05-27 17:19:06 +0900

    [Gold IV] Title: 은하철도, Time: 284 ms, Memory: 35756 KB -BaekjoonHub

--- 백준/Gold/17250. 은하철도/은하철도.py
+++ 백준/Gold/17250. 은하철도/은하철도.py
@@ -1,21 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def find_root(n):
+def find_root(n, R):
     if R[n] != n:
-        R[n] = find_root(R[n])
+        R[n] = find_root(R[n], R)
     return R[n]
 
-N, M = map(int, input().split())
-R = [i for i in range(N + 1)]
-cnt = [0] * (N + 1)
-for i in range(N):
-    cnt[i + 1] = int(input())
-for _ in range(M):
-    a, b = map(int, input().split())
-    ar = find_root(a)
-    br = find_root(b)
-    if ar != br:
-        R[max(ar, br)] = min(ar, br)
-        cnt[min(ar, br)] += cnt[max(ar, br)]
-    print(cnt[min(ar, br)])
+def solve():
+    N, M = map(int, input().split())
+    R = [i for i in range(N + 1)]
+    cnt = [0] * (N + 1)
+    for i in range(N):
+        cnt[i + 1] = int(input())
+    for _ in range(M):
+        a, b = map(int, input().split())
+        ar = find_root(a, R)
+        br = find_root(b, R)
+        if ar != br:
+            R[max(ar, br)] = min(ar, br)
+            cnt[min(ar, br)] += cnt[max(ar, br)]
+        print(cnt[min(ar, br)])
+
+solve()
