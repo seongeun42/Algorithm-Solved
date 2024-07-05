b2ba43aab4ae8fd1742348fd30a690e0fbde6e22
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-07-05 11:47:34 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,6 @@
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE SKILL_CODE & (SELECT sum(CODE)
+                    FROM SKILLCODES
+                    WHERE NAME IN ("Python", "C#")) != 0
+ORDER BY ID
