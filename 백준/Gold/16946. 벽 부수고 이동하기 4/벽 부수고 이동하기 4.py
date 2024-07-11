a7aa11fd3b99450b88c410617d889e69f3a64658
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-07-11 16:33:29 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 1280 ms, Memory: 49920 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -1,40 +1,48 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 
 dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
 
-def dfs(cr, cc, visited, num):
-    visited[cr][cc] = num
+def bfs(N, M, arr, sr, sc, visited, num):
+    q = deque([(sr, sc)])
+    visited[sr][sc] = num
     cnt = 1
-    for d in range(4):
-        nr, nc = cr + dr[d], cc + dc[d]
-        if 0 <= nr < N and 0 <= nc < M:
-            if arr[nr][nc] == '0' and visited[nr][nc] == -1:
-                cnt += dfs(nr, nc, visited, num)
+    while q:
+        cr, cc = q.popleft()
+        for d in range(4):
+            nr, nc = cr + dr[d], cc + dc[d]
+            if 0 <= nr < N and 0 <= nc < M:
+                if arr[nr][nc] == '0' and visited[nr][nc] == -1:
+                    visited[nr][nc] = num
+                    cnt += 1
+                    q.append((nr, nc))
     return cnt
 
-N, M = map(int, input().split())
-arr = [input().rstrip() for _ in range(N)]
-cnt = []
-visited = [[-1] * M for _ in range(N)]
-ans = [[0] * M for _ in range(N)]
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == '0' and visited[i][j] == -1:
-            cnt.append(dfs(i, j, visited, len(cnt)))
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == '1':
-            ans[i][j] = 1
-            chk = set()
-            for d in range(4):
-                nr, nc = i + dr[d], j + dc[d]
-                if 0 <= nr < N and 0 <= nc < M:
-                    if arr[nr][nc] == '0' and visited[nr][nc] not in chk:
-                        ans[i][j] += cnt[visited[nr][nc]]
-                        chk.add(visited[nr][nc])
-            ans[i][j] %= 10
-for v in ans:
-    print("".join(map(str, v)))
+def solve():
+    N, M = map(int, input().split())
+    arr = [input().rstrip() for _ in range(N)]
+    cnt = []
+    visited = [[-1] * M for _ in range(N)]
+    ans = [[0] * M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == '0' and visited[i][j] == -1:
+                cnt.append(bfs(N, M, arr, i, j, visited, len(cnt)))
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == '1':
+                ans[i][j] = 1
+                chk = set()
+                for d in range(4):
+                    nr, nc = i + dr[d], j + dc[d]
+                    if 0 <= nr < N and 0 <= nc < M:
+                        if arr[nr][nc] == '0' and visited[nr][nc] not in chk:
+                            ans[i][j] += cnt[visited[nr][nc]]
+                            chk.add(visited[nr][nc])
+                ans[i][j] %= 10
+    for v in ans:
+        print("".join(map(str, v)))
+
+solve()
