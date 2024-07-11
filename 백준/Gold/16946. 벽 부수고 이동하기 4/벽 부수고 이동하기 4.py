c0aea64ded7625827bfe7c9e967fd1262497fddb
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-07-11 15:31:51 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 2244 ms, Memory: 160864 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -5,34 +5,25 @@ sys.setrecursionlimit(10**7)
 dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
 
-def count(cr, cc):
-    cnt_arr[cr][cc] = -1
+def dfs(cr, cc, visited, num):
+    visited[cr][cc] = num
     cnt = 1
     for d in range(4):
         nr, nc = cr + dr[d], cc + dc[d]
         if 0 <= nr < N and 0 <= nc < M:
-            if arr[nr][nc] == '0' and cnt_arr[nr][nc] == 0:
-                cnt += count(nr, nc)
+            if arr[nr][nc] == '0' and visited[nr][nc] == -1:
+                cnt += dfs(nr, nc, visited, num)
     return cnt
-
-def fill(cr, cc, cnt, idx):
-    cnt_arr[cr][cc] = (cnt, idx)
-    for d in range(4):
-        nr, nc = cr + dr[d], cc + dc[d]
-        if 0 <= nr < N and 0 <= nc < M:
-            if arr[nr][nc] == '0' and cnt_arr[nr][nc] == -1:
-                fill(nr, nc, cnt, idx)
 
 N, M = map(int, input().split())
 arr = [input().rstrip() for _ in range(N)]
-cnt_arr = [[0] * M for _ in range(N)]
+cnt = []
+visited = [[-1] * M for _ in range(N)]
 ans = [[0] * M for _ in range(N)]
-idx = 0
 for i in range(N):
     for j in range(M):
-        if arr[i][j] == '0' and cnt_arr[i][j] == 0:
-            fill(i, j, count(i, j), idx)
-            idx += 1
+        if arr[i][j] == '0' and visited[i][j] == -1:
+            cnt.append(dfs(i, j, visited, len(cnt)))
 for i in range(N):
     for j in range(M):
         if arr[i][j] == '1':
@@ -41,9 +32,9 @@ for i in range(N):
             for d in range(4):
                 nr, nc = i + dr[d], j + dc[d]
                 if 0 <= nr < N and 0 <= nc < M:
-                    if arr[nr][nc] == '0' and cnt_arr[nr][nc][1] not in chk:
-                        ans[i][j] += cnt_arr[nr][nc][0]
-                        chk.add(cnt_arr[nr][nc][1])
+                    if arr[nr][nc] == '0' and visited[nr][nc] not in chk:
+                        ans[i][j] += cnt[visited[nr][nc]]
+                        chk.add(visited[nr][nc])
             ans[i][j] %= 10
 for v in ans:
     print("".join(map(str, v)))
