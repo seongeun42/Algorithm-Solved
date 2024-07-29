33e71a4c36ee33df1186165fe62f8502dcb78d91
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-07-29 20:19:58 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 200 ms, Memory: 39220 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -1,18 +1,22 @@
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
 def solve():
     N = int(input())
-    time = defaultdict(int)
+    start, end = [], []
     for _ in range(N):
         s, e = map(int, input().split())
-        time[s] += 1
-        time[e] -= 1
-    time = sorted(time.items())
+        start.append(s)
+        end.append(e)
+    start.sort()
+    end.sort()
     ans, cnt = 0, 0
-    for t, v in time:
-        cnt += v
+    e = 0
+    for s in start:
+        cnt += 1
+        if e < len(end) and end[e] <= s:
+            cnt -= 1
+            e += 1
         if ans < cnt:
             ans = cnt
     print(ans)
