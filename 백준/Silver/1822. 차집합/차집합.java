5b28bff8e40ec0b63b4e87f7255adcc570182857
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-08-13 18:19:50 +0900

    [Silver IV] Title: 차집합, Time: 1428 ms, Memory: 271000 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -9,18 +9,19 @@ public class Main {
             an = Integer.parseInt(st.nextToken());
             bn = Integer.parseInt(st.nextToken());
             Set<Integer> a = new TreeSet<>();
-            Set<Integer> b = new TreeSet<>();
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < an; i++) {
                 a.add(Integer.parseInt(st.nextToken()));
             }
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < bn; i++) {
-                b.add(Integer.parseInt(st.nextToken()));
+                a.remove(Integer.parseInt(st.nextToken()));
             }
-            a.removeAll(b);
-            System.out.println(a.size());
-            for (int c : a)
-                System.out.print(c + " ");
+            StringBuilder sb = new StringBuilder();
+            sb.append(a.size()).append("\n");
+            for (Integer v : a) {
+                sb.append(v).append(" ");
+            }
+            System.out.println(sb);
         }
 }
