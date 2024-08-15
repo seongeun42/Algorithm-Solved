5d3935a89dc121b51089e64b53fea2343304d27f
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-08-15 22:32:25 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.py
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.py
@@ -6,6 +6,4 @@ else:
     cnt = [i for i in range(1, K + 1)]
     N -= minimum
     N %= K
-    for i in range(1, N + 1):
-        cnt[K - i] += 1
-    print(cnt[-1] - cnt[0])
+    print(cnt[-1] - cnt[0] + (1 if N > 0 else 0))
