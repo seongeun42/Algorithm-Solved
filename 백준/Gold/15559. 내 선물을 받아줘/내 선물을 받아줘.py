28da40c71e570eb76a43258bfab10c1cc4e90f78
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-08-18 18:32:34 +0900

    [Gold II] Title: 내 선물을 받아줘, Time: 432 ms, Memory: 54840 KB -BaekjoonHub

--- 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.py
+++ 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.py
@@ -1,40 +1,31 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 
 direction = {'N': (-1, 0), 'S': (1, 0), 'W': (0, -1), 'E': (0, 1)}
+ans = 0
 
-def find_root(n, R):
-    if R[n] != n:
-        R[n] = find_root(R[n], R)
-    return R[n]
-
-def dfs(cr, cc, jido, visited, R):
-    visited[cr][cc] = 1
+def dfs(cr, cc, jido, visited, cnt):
+    global ans
+    visited[cr][cc] = cnt
     d = jido[cr][cc]
     nr, nc = cr + direction[d][0], cc + direction[d][1]
     if 0 <= nr < len(visited) and 0 <= nc < len(visited[0]):
-        c = cr * len(visited[0]) + cc
-        n = nr * len(visited[0]) + nc
-        croot = find_root(c, R)
-        nroot = find_root(n, R)
-        if croot != nroot:
-            R[max(croot, nroot)] = min(croot, nroot)
         if not visited[nr][nc]:
-            dfs(nr, nc, jido, visited, R)
+            dfs(nr, nc, jido, visited, cnt)
+        elif visited[nr][nc] == cnt:
+            ans += 1
 
 def solve():
     N, M = map(int, input().split())
     jido = [input().rstrip() for _ in range(N)]
-    R = [i for i in range(N * M)]
     visited = [[0] * M for _ in range(N)]
+    cnt = 1
     for i in range(N):
         for j in range(M):
             if not visited[i][j]:
-                dfs(i, j, jido, visited, R)
-    ans = 0
-    for i in range(N * M):
-        if R[i] == i:
-            ans += 1
+                dfs(i, j, jido, visited, cnt)
+                cnt += 1
     print(ans)
 
 solve()
