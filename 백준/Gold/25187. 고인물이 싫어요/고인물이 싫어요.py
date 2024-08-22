623f582922a2e3e410955f129ee26868e004bb35
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-08-22 22:14:42 +0900

    [Gold IV] Title: 고인물이 싫어요, Time: 312 ms, Memory: 34484 KB -BaekjoonHub

---
+++ 백준/Gold/25187. 고인물이 싫어요/고인물이 싫어요.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find_root(n, R):
+    if R[n] < 0:
+        return n
+    R[n] = find_root(R[n], R)
+    return R[n]
+
+def solve():
+    N, M, Q = map(int, input().split())
+    water = [-1] + [*map(int, input().split())]
+    R = [-1] * (N + 1)
+    cnt = water[:]
+    for _ in range(M):
+        u, v = map(int, input().split())
+        ur = find_root(u, R)
+        vr = find_root(v, R)
+        if ur < vr:
+            cnt[ur] += cnt[vr]
+            R[ur] += R[vr]
+            R[vr] = ur
+        elif ur > vr:
+            cnt[vr] += cnt[ur]
+            R[vr] += R[ur]
+            R[ur] = vr
+    for _ in range(Q):
+        K = int(input())
+        kr = find_root(K, R)
+        print(1 if -R[kr] // 2 < cnt[kr] else 0)
+
+solve()
