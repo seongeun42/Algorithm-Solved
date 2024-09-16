3cc6208d8d6b9f71f8101a50bb5e5954757b81f9
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-09-16 21:36:30 +0900

    [Bronze I] Title: 디지털 티비, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.py
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.py
@@ -5,11 +5,7 @@ n = int(input())
 ch = [input().rstrip() for _ in range(n)]
 kbs1 = ch.index("KBS1")
 kbs2 = ch.index("KBS2")
-if kbs1 == 0:
-    print("1" * kbs2 + "4" * (kbs2 - 1))
-elif kbs2 == 0:
-    print("1" * kbs1 + "4" * kbs1)
-elif kbs1 < kbs2:
+if kbs1 < kbs2:
     print("1" * kbs1 + "4" * kbs1, end="")
     print("1" * kbs2 + "4" * (kbs2 - 1))
 else:
