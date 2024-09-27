81f739117462dcccb3c905c1bb53a38ba9e0dbf3
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-09-27 15:31:27 +0900

    [Gold IV] Title: PPAP, Time: 76 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -1,27 +1,19 @@
-s = input()
-leng = len(s)
-stack = []
-isPPAP = False
-i = 0
-while i < leng:
-    if s[i] == 'P':
-        if i < leng - 4 and s[i:i+4] == "PPAP":
-            stack.append("P")
-            isPPAP = True
-            i += 4
-        elif stack and stack[-1] == 'P' and i < leng - 3 and s[i:i+3] == "PAP":
-            isPPAP = True
-            i += 3
-        else:
-            stack.append("P")
-            isPPAP = False
-            i += 1
-    elif s[i] == "A":
-        if len(stack) >= 2 and stack[-2] == "P" and stack[-1] == "P" and i < leng - 1 and s[i+1] == "P":
-            stack.pop()
-            isPPAP = True
-            i += 2
-        else:
-            isPPAP = False
-            break
-print("PPAP" if isPPAP or s == "P" else "NP")
+def check(s):
+    if 'AA' in s or s[-1] == 'A':
+        return "NP"
+    pcnt = 0
+    preA = False
+    for c in s:
+        if c == 'P':
+            if preA:
+                pcnt -= 1
+                preA = False
+            else:
+                pcnt += 1
+        elif c == 'A':
+            if pcnt < 2:
+                return "NP"
+            preA = True
+    return "PPAP" if pcnt == 1 else "NP"
+
+print(check(input()))
