5925db9f246029d2649a7c689f34145bb4419f56
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-09-28 12:58:31 +0900

    [Gold IV] Title: 괄호 제거, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.py
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.py
@@ -1,27 +1,24 @@
 def dfs(cur, pick, max_pick, s, pair, ans):
-    if len(pick) == max_pick * 2:
-        exp = []
-        for i in range(len(s)):
-            if i not in pick:
-                exp.append(s[i])
+    if len(pick) == max_pick:
+        exp = list(s)
+        for l, r in pick:
+            exp[l], exp[r] = '', ''
         ans.add("".join(exp))
         return
     for i in range(cur + 1, len(pair)):
-        pick.append(pair[i][0])
-        pick.append(pair[i][1])
+        pick.append(pair[i])
         dfs(i, pick, max_pick, s, pair, ans)
-        pick.pop()
         pick.pop()
 
 def solve():
     s = input()
     pair = []
-    parentthese = []
+    brackets = []
     for i in range(len(s)):
         if s[i] == "(":
-            parentthese.append(i)
+            brackets.append(i)
         elif s[i] == ")":
-            pair.append((parentthese.pop(), i))
+            pair.append((brackets.pop(), i))
     ans = set()
     for i in range(1, len(pair) + 1):
         dfs(-1, [], i, s, pair, ans)
