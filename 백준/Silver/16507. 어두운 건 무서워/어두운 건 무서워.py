5b26403c85eb333ef0c15385d3640dcedd81f5b3
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-10-05 23:16:20 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 400 ms, Memory: 70256 KB -BaekjoonHub

--- 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
@@ -3,15 +3,16 @@ input = sys.stdin.readline
 
 def solve():
     R, C, Q = map(int, input().split())
-    picture = [[*map(int, input().split())] for _ in range(R)]
-    prefix = [[0] * (C + 1) for _ in range(R + 1)]
+    picture = [[0] * (C + 1)]
+    for _ in range(R):
+        picture.append([0] + [*map(int, input().split())])
     for i in range(1, R + 1):
         for j in range(1, C + 1):
-            prefix[i][j] = picture[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1]
+            picture[i][j] += picture[i - 1][j] + picture[i][j - 1] - picture[i - 1][j - 1]
     for _ in range(Q):
         r1, c1, r2, c2 = map(int, input().split())
         cnt = (r2 - r1 + 1) * (c2 - c1 + 1)
-        hap = prefix[r2][c2] - prefix[r1 - 1][c2] - prefix[r2][c1 - 1] + prefix[r1 - 1][c1 - 1]
+        hap = picture[r2][c2] - picture[r1 - 1][c2] - picture[r2][c1 - 1] + picture[r1 - 1][c1 - 1]
         print(hap // cnt)
 
 solve()
