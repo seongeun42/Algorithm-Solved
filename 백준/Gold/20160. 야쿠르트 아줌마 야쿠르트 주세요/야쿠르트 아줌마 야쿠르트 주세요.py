20ea2e9a412c8c06ae5b9f1810c4a0f003896acf
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-10-15 16:23:54 +0900

    [Gold III] Title: 야쿠르트 아줌마 야쿠르트 주세요, Time: 2256 ms, Memory: 60100 KB -BaekjoonHub

---
+++ 백준/Gold/20160. 야쿠르트 아줌마 야쿠르트 주세요/야쿠르트 아줌마 야쿠르트 주세요.py
@@ -0,0 +1,53 @@
+import sys, heapq
+input = sys.stdin.readline
+
+def dijkstra(G, weight, S):
+    dp = [1e10] * len(G)
+    dp[S] = 0
+    hq = [(0, S)]
+    while hq:
+        cw, cn = heapq.heappop(hq)
+        if dp[cn] < cw:
+            continue
+        for nn in G[cn]:
+            w = weight[(min(nn, cn), max(nn, cn))] + cw
+            if w < dp[nn]:
+                dp[nn] = w
+                heapq.heappush(hq, (w, nn))
+    return dp
+
+def solve():
+    V, E = map(int, input().split())
+    G = [[] for _ in range(V + 1)]
+    weight = {}
+    for _ in range(E):
+        u, v, w = map(int, input().split())
+        G[u].append(v)
+        G[v].append(u)
+        tmp = (min(u, v), max(u, v))
+        if tmp not in weight or w < weight[tmp]:
+            weight[tmp] = w
+    seller = [*map(int, input().split())]
+    start = int(input())
+    my_time = dijkstra(G, weight, start)
+    seller_time = [1e10] * (V + 1)
+    seller_time[seller[0]] = 0
+    ans = []
+    if my_time[seller[0]] <= 0:
+        ans.append(seller[0])
+    time = 0
+    pre = 0
+    while pre < 10:
+        dp = dijkstra(G, weight, seller[pre])
+        nxt = pre + 1
+        while nxt < 10 and dp[seller[nxt]] == 1e10:
+            nxt += 1
+        if nxt == 10:
+            break
+        time += dp[seller[nxt]]
+        pre = nxt
+        if my_time[seller[nxt]] <= time:
+            ans.append(seller[nxt])
+    print(min(ans) if ans else -1)
+
+solve()
