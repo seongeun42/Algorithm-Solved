6f2375372e53471cd8fd885ebefb1d5199c51225
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-10-21 22:06:27 +0900

    [Gold V] Title: 아우으 우아으이야!!, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
@@ -3,14 +3,14 @@ input = sys.stdin.readline
 
 def solve():
     N = int(input())
-    coordi = sorted([[*map(int, input().split())] for _ in range(N)])
     ans = 0
-    s, e = coordi[0]
-    for x, y in coordi[1:]:
+    s, e = map(int, input().split())
+    for _ in range(N - 1):
+        x, y = map(int, input().split())
         if e < x:
             ans += e - s
-            s, e = x, y
-        elif e >= x and e < y:
+            s = x
+        if e < y:
             e = y
     ans += e - s
     print(ans)
