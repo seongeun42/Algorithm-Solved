c47d734f1d974cbc839b8536425a7e0a9212bba7
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-11-12 21:53:05 +0900

    [Bronze I] Title: DNA 해독, Time: 192 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Bronze/1672. DNA 해독/DNA 해독.py
@@ -0,0 +1,11 @@
+N = int(input())
+S = input()
+num = {'A': 0, 'G': 1, 'C': 2, 'T': 3}
+change = [[0, 2, 0, 1],
+          [2, 1, 3, 0],
+          [0, 3, 2, 1],
+          [1, 0, 1, 3]]
+last = num[S[-1]]
+for c in range(N - 2, -1, -1):
+    last = change[num[S[c]]][last]
+print("AGCT"[last])
