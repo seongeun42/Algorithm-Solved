1c5b1dd2aa88189bd31d62c39a00a96bde53873a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-11-14 22:52:24 +0900

    [Gold IV] Title: 일감호에 다리 놓기, Time: 2396 ms, Memory: 336532 KB -BaekjoonHub

--- 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.py
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.py
@@ -12,6 +12,7 @@ def solve():
     need = sorted(list(zip([*map(int, input().split())], range(1, N + 1))))
     R = [0] + list(range(N))
     R[1] = 1
+    connect = 1
     last_connected = True
     for _ in range(M):
         a, b = map(int, input().split())
@@ -19,21 +20,22 @@ def solve():
         if (a, b) == (1, N):
             last_connected = False
         R[b] = b
+        connect += 1
     if last_connected:
         nr = find_root(N, R)
         R[nr] = 1
+        connect -= 1
     if M <= 1:
         print("YES")
         return
     total = 0
-    connect = 0
     for cnt, num in need:
         root = find_root(num, R)
         if root != 0:
             R[root] = 0
             total += cnt
-            connect += 1
-        if connect > M:
+            connect -= 1
+        if connect == 0:
             break
     print("YES" if total <= K else "NO")
 
