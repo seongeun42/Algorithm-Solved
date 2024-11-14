b8db2ccc357106a0e0009a39386dd7e281fe98e5
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-11-14 22:27:32 +0900

    [Gold IV] Title: 일감호에 다리 놓기, Time: 2604 ms, Memory: 337556 KB -BaekjoonHub

---
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+
+def find_root(n, R):
+    if R[n] != n:
+        R[n] = find_root(R[n], R)
+    return R[n]
+
+def solve():
+    N, M, K = map(int, input().split())
+    need = sorted(list(zip([*map(int, input().split())], range(1, N + 1))))
+    R = [0] + list(range(N))
+    R[1] = 1
+    last_connected = True
+    for _ in range(M):
+        a, b = map(int, input().split())
+        a, b = min(a, b), max(a, b)
+        if (a, b) == (1, N):
+            last_connected = False
+        R[b] = b
+    if last_connected:
+        nr = find_root(N, R)
+        R[nr] = 1
+    if M <= 1:
+        print("YES")
+        return
+    total = 0
+    connect = 0
+    for cnt, num in need:
+        root = find_root(num, R)
+        if root != 0:
+            R[root] = 0
+            total += cnt
+            connect += 1
+        if connect > M:
+            break
+    print("YES" if total <= K else "NO")
+
+solve()
