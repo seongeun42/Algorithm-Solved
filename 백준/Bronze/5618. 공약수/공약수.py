0b62905e4106ccbcee5e59e20942747d36649048
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-11-19 21:18:06 +0900

    [Bronze II] Title: 공약수, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.py
+++ 백준/Bronze/5618. 공약수/공약수.py
@@ -5,7 +5,10 @@ nums = [*map(int, input().split())]
 gcd = math.gcd(nums[0], nums[1])
 if n == 3:
     gcd = math.gcd(gcd, nums[2])
-for i in range(1, gcd // 2 + 1):
+ans = set()
+for i in range(1, int(gcd ** 0.5)+1):
     if gcd % i == 0:
-        print(i)
-print(gcd)
+        ans.add(i)
+        ans.add(gcd // i)
+for i in sorted(ans):
+    print(i)
