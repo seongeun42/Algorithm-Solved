4b2732632fa6773d375d3003399fbf5ab48af490
Seongeun <64778589+seongeun42@users.noreply.github.com>
2024-12-16 22:08:42 +0900

    [Silver III] Title: 에너지 드링크, Time: 80 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.py
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.py
@@ -1,6 +1,4 @@
 N = int(input())
 drink = sorted([*map(int, input().split())])
-ans = drink.pop()
-for i in range(N - 1):
-    ans += drink[i] / 2
+ans = drink.pop() + (sum(drink) / 2)
 print(int(ans) if ans % 1  == 0 else ans)
