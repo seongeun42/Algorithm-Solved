a6e8f874134c5d671a690a3f91bfe3cf974bf388
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-01-08 00:52:58 +0900

    [Gold V] Title: 별자리 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/5588. 별자리 찾기/별자리 찾기.py
+++ 백준/Gold/5588. 별자리 찾기/별자리 찾기.py
@@ -3,13 +3,11 @@ input = sys.stdin.readline
 
 def solve():
     m = int(input())
-    target = {tuple([*map(int, input().split())]) for _ in range(m)}
+    target = [[*map(int, input().split())] for _ in range(m)]
     n = int(input())
     star = {tuple([*map(int, input().split())]) for _ in range(n)}
-    target_tmp = target.pop()
-    target.add(target_tmp)
     for sx, sy in star:
-        x_diff, y_diff = sx - target_tmp[0], sy - target_tmp[1]
+        x_diff, y_diff = sx - target[0][0], sy - target[0][1]
         for tx, ty in target:
             nx, ny = tx + x_diff, ty + y_diff
             if (nx, ny) not in star:
