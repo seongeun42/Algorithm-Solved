1f021d0b1c9333befc3d2b22ae16d0951ea0123a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-03-06 23:16:56 +0900

    [Bronze III] Title: 완전 세제곱, Time: 1536 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.py
@@ -0,0 +1,9 @@
+for a in range(2, 101):
+    for b in range(2, 101):
+        for c in range(b + 1, 101):
+            for d in range(c + 1, 101):
+                a3, bcd3 = a**3, b**3 + c**3 + d**3
+                if a3 == bcd3:
+                    print(f"Cube = {a}, Triple = ({b},{c},{d})")
+                elif a3 < bcd3:
+                    break
