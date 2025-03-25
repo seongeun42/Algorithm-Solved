7d57a272e86455f6105779dfcfdba8c2d222500d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-03-25 11:57:02 +0900

    [Gold IV] Title: 탈출, Time: 108 ms, Memory: 37408 KB -BaekjoonHub

--- 백준/Gold/16397. 탈출/탈출.py
+++ 백준/Gold/16397. 탈출/탈출.py
@@ -2,27 +2,28 @@ from collections import deque
 
 def solve():
     N, T, G = map(int, input().split())
-    visited = {N}
-    q = deque([(N, 0)])
+    visited = [-1] * 100_000
+    visited[N] = 0
+    q = deque([N])
     while q:
-        num, cnt = q.popleft()
+        num = q.popleft()
         if num == G:
-            print(cnt)
+            print(visited[G])
             return
-        if cnt < T:
+        if visited[num] < T:
             A = num + 1
-            if A <= 99999 and A not in visited:
-                visited.add(A)
-                q.append((A, cnt + 1))
+            if A <= 99999 and visited[A] == -1:
+                visited[A] = visited[num] + 1
+                q.append(A)
             if num != 0:
                 B = num * 2
                 if B <= 99999:
                     B = list(str(B))
                     B[0] = str(int(B[0]) - 1)
                     B = int(''.join(B))
-                    if B not in visited:
-                        visited.add(B)
-                        q.append((B, cnt + 1))
+                    if visited[B] == -1:
+                        visited[B] = visited[num] + 1
+                        q.append(B)
     print("ANG")
     return
 
