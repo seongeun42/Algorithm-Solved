cf154d9c7faefb557e63d27f1d4c52a6a9d9000a
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-03-27 17:32:04 +0900

    [Gold IV] Title: 회사 문화 1, Time: 140 ms, Memory: 43484 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,28 +1,15 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-def dfs(cur, tree, praise, visited):
-    visited[cur] = True
-    for nxt in tree[cur]:
-        if not visited[nxt]:
-            praise[nxt] += praise[cur]
-            dfs(nxt, tree, praise, visited)
 
 def solve():
     n, m = map(int, input().split())
-    boss = [*map(int, input().split())]
-    tree = [[] for _ in range(n + 1)]
-    for i in range(n):
-        tree[boss[i]].append(i + 1)
+    boss = [0] + [*map(int, input().split())]
     praise = [0] * (n + 1)
     for _ in range(m):
         i, w = map(int, input().split())
         praise[i] += w
-    visited = [False] * (n + 1)
-    for i in range(1, n + 1):
-        if not visited[i]:
-            dfs(i, tree, praise, visited)
+    for i in range(2, n + 1):
+        praise[i] += praise[boss[i]]
     print(*praise[1:])
 
 solve()
