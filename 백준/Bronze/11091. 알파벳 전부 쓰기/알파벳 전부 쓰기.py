200bd435ec15a24ec24a77f13f3226b7bce805df
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-04-03 22:16:18 +0900

    [Bronze II] Title: 알파벳 전부 쓰기, Time: 112 ms, Memory: 36972 KB -BaekjoonHub

---
+++ 백준/Bronze/11091. 알파벳 전부 쓰기/알파벳 전부 쓰기.py
@@ -0,0 +1,16 @@
+import re
+
+def use_all_alphabet(string, alphabets):
+    is_pangram = "pangram"
+    string = re.sub(r"[^a-z]", '', string.lower())
+    string = set(string)
+    not_uesd_alphabet = alphabets - string
+    if not_uesd_alphabet != set():
+        not_uesd_alphabet = sorted(list(not_uesd_alphabet))
+        is_pangram = f"missing {''.join(not_uesd_alphabet)}"    
+    return is_pangram
+
+alphabets = set([chr(i) for i in range(97, 123)])
+for _ in range(int(input())):
+    string = input()
+    print(use_all_alphabet(string, alphabets))
