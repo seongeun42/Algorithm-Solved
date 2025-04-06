42fc9950afbcf78d41ba27c1f54d9ce97a34f53c
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-04-06 16:31:56 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 608 ms, Memory: 60424 KB -BaekjoonHub

--- 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 def solve():
     N, Q = map(int, input().split())
     B = sorted([*map(int, input().split())])
-    prefixed = B[:]
-    for i in range(1, N):
-        prefixed[i] += prefixed[i - 1]
+    prefixed = [0]
+    for i in range(N):
+        prefixed.append(B[i] + prefixed[i])
     for _ in range(Q):
         L, R = map(int, input().split())
-        print(prefixed[R - 1] - prefixed[L - 1] + B[L - 1])
+        print(prefixed[R] - prefixed[L - 1])
 
 solve()
