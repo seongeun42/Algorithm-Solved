2c8e0fa529334ec6feac3cd65d17e8269f2dc5f8
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-04-07 14:27:14 +0900

    [Silver III] Title: 눈 치우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/26215. 눈 치우기/눈 치우기.py
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.py
@@ -1,15 +1,12 @@
-import heapq
-
 N = int(input())
-snow = [-v for v in [*map(int, input().split())]]
-heapq.heapify(snow)
+snow = sorted([*map(int, input().split())])
 ans = 0
 while snow:
     if len(snow) == 1:
-        ans += -heapq.heappop(snow)
+        ans += snow.pop()
     else:
-        a = -heapq.heappop(snow)
-        b = -heapq.heappop(snow)
-        ans += b
-        heapq.heappush(snow, -(a - b))
+        b, s = snow.pop(), snow.pop()
+        ans += s
+        snow.append(b - s)
+        snow.sort()
 print(ans if ans <= 1440 else -1)
