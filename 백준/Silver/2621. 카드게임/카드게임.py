acdacaaca9722c6767f068c5c0d660257f913f06
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-04-08 18:06:54 +0900

    [Silver III] Title: 카드게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2621. 카드게임/카드게임.py
+++ 백준/Silver/2621. 카드게임/카드게임.py
@@ -1,43 +1,39 @@
+def is_continuous_chk(nums):
+    if len(nums) < 5:
+        return False
+    tmp = sorted(list(nums))
+    for i in range(1, 5):
+        if tmp[i] - tmp[i - 1] != 1:
+            return False
+    return True
+
 def numbers(num):
+    max_num = 0
     four, three = 0, 0
     two = []
-    is_continuous = True
-    max_num = 0
-    total = 0
     for i in range(1, 10):
-        if max_num == 0 and num[i] > 0:
-            max_num = i
         if num[i] == 4:
-            return False, i, i, 0, []
+            four = i
         elif num[i] == 3:
             three = i
-            is_continuous = False
-            total += 3
         elif num[i] == 2:
             two.append(i)
-            is_continuous = False
-            total += 2
-        elif num[i] == 1:
-            if is_continuous and (max_num != i and max_num != i - 1):
-                is_continuous = False
-            total += 1
-        elif num[i] == 0:
-            if max_num > 0:
-                is_continuous = False
-            continue
-        max_num = i
-        if total == 5:
-            return is_continuous, max_num, four, three, two
+        if num[i] > 0:
+            max_num = i
+    return max_num, four, three, two
 
 def solve():
     card = {'R': 0, 'B': 0, 'Y': 0, 'G': 0}
     num = [0] * 10
+    nums = set()
     for _ in range(5):
         c, n = input().split()
         n = int(n)
         card[c] += 1
         num[n] += 1
-    is_continuous, max_num, four, three, two = numbers(num)
+        nums.add(n)
+    is_continuous = is_continuous_chk(nums)
+    max_num, four, three, two = numbers(num)
     if 5 in card.values() and is_continuous:
         print(max_num + 900)
     elif four > 0:
