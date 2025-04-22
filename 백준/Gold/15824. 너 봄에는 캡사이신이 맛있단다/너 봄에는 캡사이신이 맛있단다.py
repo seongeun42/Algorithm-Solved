5a7cc2a2ab1ca3653917949767ecc030c1b48bb9
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-04-22 14:58:09 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 2356 ms, Memory: 66624 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,10 +1,20 @@
-N = int(input())
-scoville = sorted([*map(int, input().split())])
-prefix = [scoville[0]]
-for i in range(1, N):
-    prefix.append(prefix[i - 1] + scoville[i])
-total = 0
-for i in range(N - 1):
-    diff = prefix[-1] - prefix[i] - prefix[N - i - 2]
-    total += (diff * (2 ** i)) % 1_000_000_007
-print(total % 1_000_000_007)
+import sys
+MOD = 1000000007
+ 
+def pow(a, b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    
+    half = pow(a, b//2)
+    return half * half % MOD if b % 2 == 0 else half * half * a % MOD
+ 
+N = int(sys.stdin.readline())
+arr = sorted(list(map(int, sys.stdin.readline().split())))
+answer = 0
+ 
+for i in range(N):
+    answer += arr[i] * (pow(2, i) - pow(2, N - i - 1))
+ 
+print(answer % MOD)
