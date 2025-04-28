f499db3979114918729e17fef3425c25e98bcf7f
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-04-28 17:08:07 +0900

    [Gold IV] Title: 좋은 친구, Time: 140 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.py
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.py
@@ -1,20 +1,17 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 def solve():
     N, K = map(int, input().split()) 
-    names = deque([input().rstrip() for _ in range(N)])
+    names = [len(input().rstrip()) for _ in range(N)]
     ans = 0
     cnt = [0] * 21
     for i in range(K + 1):
-        cnt[len(names[i])] += 1
-    for i in range(21):
-        ans += cnt[i] * (cnt[i] - 1) // 2
-    while len(names) > K + 1:
-        cnt[len(names[0])] -= 1
-        names.popleft()
-        name = len(names[K])
+        ans += cnt[names[i]]
+        cnt[names[i]] += 1
+    for i in range(N - K - 1):
+        cnt[names[i]] -= 1
+        name = names[i + K + 1]
         ans += cnt[name]
         cnt[name] += 1
     print(ans)
