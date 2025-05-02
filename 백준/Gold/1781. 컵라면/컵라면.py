12bfb891d109854f9e0f632bb0d15cb61cbd8aca
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-05-02 11:58:57 +0900

    [Gold II] Title: 컵라면, Time: 556 ms, Memory: 76932 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.py
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -5,15 +5,14 @@ def solve():
     N = int(input())
     arr = sorted([[*map(int, input().split())] for _ in range(N)])
     hq = []
-    day, ans = 1, 0
+    day = 1
     for dead, cup in arr:
         if day <= dead:
-            ans += cup
             heapq.heappush(hq, cup)
             day += 1
         elif hq[0] < cup:
-            ans = ans - heapq.heappop(hq) + cup
+            heapq.heappop(hq)
             heapq.heappush(hq, cup)
-    print(ans)
+    print(sum(hq))
 
 solve()
