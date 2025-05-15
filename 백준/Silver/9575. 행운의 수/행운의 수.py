c171bb23193bf2253a0f3d163c1e1beeca9dd4b2
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-05-15 12:28:37 +0900

    [Silver IV] Title: 행운의 수, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9575. 행운의 수/행운의 수.py
+++ 백준/Silver/9575. 행운의 수/행운의 수.py
@@ -1,23 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+def is_lucky(num):
+    while num > 0:
+        if num % 10 not in {5, 8}:
+            return False
+        num //= 10
+    return True
+
 def solve():
     T = int(input())
     for _ in range(T):
         N = int(input())
-        A = list(set([*map(int, input().split())]))
+        A = [*map(int, input().split())]
         M = int(input())
-        B = list(set([*map(int, input().split())]))
+        B = [*map(int, input().split())]
         K = int(input())
-        C = list(set([*map(int, input().split())]))
-        lucky = set()
-        for a in A:
-            for b in B:
-                for c in C:
-                    e = a + b + c
-                    tmp = set(list(str(a + b + c)))
-                    if len(tmp - {'5', '8'}) == 0:
-                        lucky.add(e)
+        C = [*map(int, input().split())]
+        lucky = set([a + b + c for c in C for b in B for a in A if is_lucky(a + b + c)])
         print(len(lucky))
 
 solve()
