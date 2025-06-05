e036f1bf386095ebe31744af0ad9c04c9d3562cd
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-06-05 11:46:08 +0900

    [Gold I] Title: 복제 로봇, Time: 44 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/1944. 복제 로봇/복제 로봇.py
+++ 백준/Gold/1944. 복제 로봇/복제 로봇.py
@@ -1,53 +1,37 @@
-from collections import deque
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
-def find_root(n, R):
-    if R[n] != n:
-        R[n] = find_root(R[n], R)
-    return R[n]
-
-def bfs(arr, start, edges, N, M, ids):
+def find_count(arr, start, N, M):
     dr = [0, 0, 1, -1]
     dc = [1, -1, 0, 0]
-    q = deque([start])
-    visited = [[0] * N for _ in range(N)]
-    visited[start[0]][start[1]] = 1
+    hq = [(0, start[0], start[1])]
+    visited = [[N * N] * N for _ in range(N)]
+    visited[start[0]][start[1]] = 0
+    connected = {start}
     cnt = 0
-    while q:
-        cr, cc = q.popleft()
-        if (cr, cc) != start and arr[cr][cc] in {'S', 'K'}:
-            edges.append((visited[cr][cc] - 1, ids[start], ids[(cr, cc)]))
-            cnt += 1
+    while hq:
+        c_dist, cr, cc = heapq.heappop(hq)
+        if arr[cr][cc] == 'K' and (cr, cc) not in connected:
+            heapq.heappush(hq, (0, cr, cc))
+            connected.add((cr, cc))
+            cnt += c_dist
+        if len(connected) == M + 1:
+            return cnt
         for d in range(4):
             nr, nc = cr + dr[d], cc + dc[d]
-            if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and arr[nr][nc] != '1':
-                visited[nr][nc] = visited[cr][cc] + 1
-                q.append((nr, nc))
-    return cnt == M
+            if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] != '1':
+                if visited[nr][nc] > c_dist + 1:
+                    visited[nr][nc] = c_dist + 1
+                    heapq.heappush(hq, (c_dist + 1, nr, nc))
+                        
+    return -1
 
 def solve():
     N, M = map(int, input().split())
     arr = [input().rstrip() for _ in range(N)]
-    ids = {}
-    edges = []
     for i in range(N):
         for j in range(N):
-            if arr[i][j] in {'S', 'K'}:
-                ids[(i, j)] = len(ids)
-    for start in ids.keys():
-        if not bfs(arr, start, edges, N, M, ids):
-            print(-1)
-            return
-    R = list(range(len(ids)))
-    edges.sort()
-    ans = 0
-    for dist, u, v in edges:
-        ur = find_root(u, R)
-        vr = find_root(v, R)
-        if ur != vr:
-            R[max(ur, vr)] = min(ur, vr)
-            ans += dist
-    print(ans)
+            if arr[i][j] == 'S':
+                return find_count(arr, (i, j), N, M)
 
-solve()
+print(solve())
