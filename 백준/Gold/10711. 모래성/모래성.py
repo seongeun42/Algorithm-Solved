c945429aa51aed5b738573bf3557c5dec2b3e159
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-07-03 17:52:52 +0900

    [Gold II] Title: 모래성, Time: 1988 ms, Memory: 50328 KB -BaekjoonHub

--- 백준/Gold/10711. 모래성/모래성.py
+++ 백준/Gold/10711. 모래성/모래성.py
@@ -1,42 +1,36 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
+
+def bfs(sand, H, W):
+    dr = [0, 0, 1, -1, 1, -1, 1, -1]
+    dc = [1, -1, 1, -1, -1, 1, 0, 0]
+    q = deque([])
+    # 빈공간 큐에 추가
+    for i in range(H):
+        for j in range(W):
+            if sand[i][j] == '.':
+                # r, c, wave_cnt
+                q.append((i, j, 0))
+    cnts = [[0] * W for _ in range(H)]
+    ans = 0
+    while q:
+        cr, cc, wave_cnt = q.popleft()
+        if ans < wave_cnt:
+            ans = wave_cnt
+        for d in range(8):
+            nr, nc = cr + dr[d], cc + dc[d]
+            if 0 <= nr < H and 0 <= nc < W and sand[nr][nc] != '.':
+                cnts[nr][nc] += 1
+                # 새로운 빈공간 큐에 추가
+                if int(sand[nr][nc]) == cnts[nr][nc]:
+                    sand[nr][nc] = '.'
+                    q.append((nr, nc, wave_cnt + 1))
+    return ans
 
 def solve():
     H, W = map(int, input().split())
     sand = [list(input().rstrip()) for _ in range(H)]
-    dr = [0, 0, 1, -1, 1, -1, 1, -1]
-    dc = [1, -1, 1, -1, -1, 1, 0, 0]
-    # 초기 모래성 인접 빈공간 개수 기록
-    cnt = {}
-    # 초기에 부서질 모래성
-    broken = []
-    for i in range(H):
-        for j in range(W):
-            if sand[i][j] in ".9":
-                continue
-            if (i, j) not in cnt:
-                cnt[(i, j)] = 0
-            for d in range(8):
-                nr, nc = i + dr[d], j + dc[d]
-                if 0 <= nr < H and 0 <= nc < W and sand[nr][nc] == '.':
-                    cnt[(i, j)] += 1
-            if int(sand[i][j]) <= cnt[(i, j)]:
-                broken.append((i, j))
-    ans = 0
-    while broken:
-        ans += 1
-        new_broken = []
-        # 모래성 부수기
-        for r, c in broken:
-            sand[r][c] = '.'
-            for d in range(8):
-                nr, nc = r + dr[d], c + dc[d]
-                if 0 <= nr < H and 0 <= nc < W and sand[nr][nc] not in ".9":
-                    cnt[(nr, nc)] += 1
-                    # 다음에 부서질 모래성 추가
-                    if cnt[(nr, nc)] == int(sand[nr][nc]):
-                        new_broken.append((nr, nc))
-        broken = new_broken
-    print(ans)
+    print(bfs(sand, H, W))
 
 solve()
