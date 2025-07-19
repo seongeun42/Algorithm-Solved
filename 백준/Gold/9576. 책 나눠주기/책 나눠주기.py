eca65fa44dc62bee09af82926b0acf35735ed12f
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-07-19 23:35:26 +0900

    [Gold II] Title: 책 나눠주기, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
@@ -1,35 +1,28 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-def dfs(x, visited, ab, book):
-    # x 학생의 책 신청 범위
-    s, e = ab[x]
-    for i in range(s - 1, e):
-        # 체크한 적 있으면 패스
-        if visited[i]:
-            continue
-        # 없으면 체크
-        visited[i] = True
-        # i번 책이 남아 있거나, i번 책 가져간 다른 학생이 책을 바꿀 수 있으면 책 주기
-        if book[i] == -1 or dfs(book[i], visited, ab, book):
-            book[i] = x
-            return True
-    return False
+def find_root(n, R):
+    if R[n] != n:
+        R[n] = find_root(R[n], R)
+    return R[n]
 
 def solve():
     T = int(input())
     for _ in range(T):
         N, M = map(int, input().split())
-        # 책이 누구에게 갔는지
-        book = [-1] * (N + 1)
-        # 범위 좁은 순으로 정렬
-        ab = sorted([[*map(int, input().split())] for _ in range(M)], key=lambda x: (x[1] - x[0]))
-        matched = 0
+        ab = sorted([[*map(int, input().split())] for _ in range(M)], key=lambda x: x[1])
+        # 0~i번까지 범위 내에서 가져갈 수 있는 책 중 가장 큰 값 기록
+        R = [i for i in range(N + 2)]
+        ans = 0
         for i in range(M):
-            visited = [False] * (N + 1)
-            if dfs(i, visited, ab, book):
-                matched += 1
-        print(matched)
+            s, e = ab[i]
+            # 앞에서부터 가져갈 수 있는 책 찾기
+            enable_book = find_root(s, R)
+            # 신청 범위 내면 책 주기
+            if s <= enable_book <= e:
+                ans += 1
+                # 준 책의 다음 번호 기록
+                R[enable_book] += 1
+        print(ans)
 
 solve()
