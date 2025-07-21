700ee69b4de913b31ffa9fb31411c028d6e72d01
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-07-21 18:14:27 +0900

    [Silver III] Title: 슬라임 합치기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.py
+++ 백준/Silver/14241. 슬라임 합치기/슬라임 합치기.py
@@ -1,11 +1,12 @@
-import heapq
+def solve():
+    N = int(input())
+    size = [*map(int, input().split())]
+    score = 0
+    while len(size) > 1:
+        size.sort()
+        a, b = size.pop(), size.pop()
+        score += a * b
+        size.append(a + b)
+    print(score)
 
-N = int(input())
-size = [-v for v in map(int, input().split())]
-heapq.heapify(size)
-score = 0
-while len(size) > 1:
-    a, b = heapq.heappop(size), heapq.heappop(size)
-    score += a * b
-    heapq.heappush(size, a + b)
-print(score)
+solve()
