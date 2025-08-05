9cf9db26bc6b0eb3ec6b4fc4ddfd79cd919b6802
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-08-05 22:24:58 +0900

    [Gold II] Title: 아맞다우산, Time: 108 ms, Memory: 35596 KB -BaekjoonHub

---
+++ 백준/Gold/17244. 아맞다우산/아맞다우산.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(N, M, home, start, end, stuff):
+    dr = [0, 0, 1, -1]
+    dc = [1, -1, 0, 0]
+    visited = [[[0] * (2 ** len(stuff)) for _ in range(N)] for _ in range(M)]
+    visited[start[0]][start[1]][0] = 1
+    q = deque([(*start, 0)])
+    while q:
+        cr, cc, cstuff = q.popleft()
+        for d in range(4):
+            nr, nc = cr + dr[d], cc + dc[d]
+            if 0 <= nr < M and 0 <= nc < N and home[nr][nc] != '#' and not visited[nr][nc][cstuff]:
+                nstuff = cstuff
+                if home[nr][nc] == 'X':
+                    nstuff = cstuff | (1 << stuff[(nr, nc)])
+                visited[nr][nc][nstuff] = visited[cr][cc][cstuff] + 1
+                q.append((nr, nc, nstuff))
+    return visited[end[0]][end[1]][-1] - 1
+
+def solve():
+    N, M = map(int, input().split())
+    home = [input().rstrip() for _ in range(M)]
+    start, end = None, None
+    stuff = {}
+    for i in range(M):
+        for j in range(N):
+            if home[i][j] == 'X':
+                stuff[(i, j)] = len(stuff)
+            elif home[i][j] == 'S':
+                start = (i, j)
+            elif home[i][j] == 'E':
+                end = (i, j)
+    print(bfs(N, M, home, start, end, stuff))
+
+solve()
