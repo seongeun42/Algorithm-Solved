ae3d51ec41001e0786bbe129b59bb41ffff8037e
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-08-11 17:42:43 +0900

    [Gold II] Title: 환승, Time: 584 ms, Memory: 117388 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.py
+++ 백준/Gold/5214. 환승/환승.py
@@ -3,20 +3,24 @@ import sys
 input = sys.stdin.readline
 
 def bfs(hyper, station, N):
+    if N == 1:
+        return 1
     q = deque([])
-    visited = [0] * len(hyper)
-    for v in station[1]:
-        q.append((v, 1))
-        visited[v] = 1
+    visited_h = [False] * len(hyper)
+    visited_s = [0] * (len(station) + 1)
+    visited_s[1] = 1
+    q.append(1)
     while q:
-        cur_h, cur_s = q.popleft()
-        if N in hyper[cur_h]:
-            return visited[cur_h] + 1 if cur_s != N else visited[cur_h]
-        for nxt_s in hyper[cur_h]:
-            for nxt_h in station[nxt_s]:
-                if not visited[nxt_h]:
-                    q.append((nxt_h, nxt_s))
-                    visited[nxt_h] = visited[cur_h] + 1
+        cur_s = q.popleft()
+        for nxt_h in station[cur_s]:
+            if not visited_h[nxt_h]:
+                visited_h[nxt_h] = True
+                for nxt_s in hyper[nxt_h]:
+                    if nxt_s == N:
+                        return visited_s[cur_s] + 1
+                    if not visited_s[nxt_s]:
+                        q.append(nxt_s)
+                        visited_s[nxt_s] = visited_s[cur_s] + 1
     return -1
 
 def solve():
@@ -24,7 +28,7 @@ def solve():
     hyper = []
     station = [[] for _ in range(N + 1)]
     for _ in range(M):
-        hyper.append(set([*map(int, input().split())]))
+        hyper.append([*map(int, input().split())])
         for v in hyper[-1]:
             station[v].append(len(hyper) - 1)
     print(bfs(hyper, station, N))
