b3c29f0e86ebf0d302118f30d5e2ee7c1c0ef6ed
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-08-31 22:56:56 +0900

    [Gold IV] Title: 소트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -1,15 +1,21 @@
-N = int(input())
-arr = [*map(int, input().split())]
-S = int(input())
-for i in range(N - 1):
-    if S == 0:
-        break
-    max_v, idx = arr[i], i
-    for j in range(i + 1, min(N, i + S + 1)):
-        if arr[j] > max_v:
-            max_v, idx = arr[j], j
-    S -= idx - i
-    for j in range(idx, i, - 1):
-        arr[j] = arr[j - 1]
-    arr[i] = max_v
-print(*arr)
+import sys
+input = sys.stdin.readline
+
+def solve():
+    N = int(input())
+    arr = [*map(int, input().split())]
+    S = int(input())
+    for i in range(N - 1):
+        if S == 0:
+            break
+        max_v, idx = arr[i], i
+        for j in range(i + 1, min(N, i + S + 1)):
+            if arr[j] > max_v:
+                max_v, idx = arr[j], j
+        S -= idx - i
+        for j in range(idx, i, - 1):
+            arr[j] = arr[j - 1]
+        arr[i] = max_v
+    print(*arr)
+
+solve()
