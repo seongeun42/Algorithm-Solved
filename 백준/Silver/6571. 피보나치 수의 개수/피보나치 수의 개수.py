2be1653d24ca3b00a066700b9f071cc35a25a854
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-09-23 22:50:29 +0900

    [Silver III] Title: 피보나치 수의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.py
+++ 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.py
@@ -1,15 +1,19 @@
-dp = [0 for i in range(1001)]
-dp[1] = 1
-dp[2] = 2
-for i in range(3, 1001) :
-    dp[i] = dp[i-1] + dp[i-2]
+import sys
+input = sys.stdin.readline
 
-while(True):
-    a, b = map(int,input().split())
-    if a == 0 and b == 0 :
+fi = [1, 2, 3]
+
+while fi[-1] <= 10 ** 100:
+    fi.append(fi[-1] + fi[-2])
+
+while 1:
+    a, b = map(int, input().split())
+    if a == b == 0:
         break
     cnt = 0
-    for i in range(1, 1001) :
-        if a <= dp[i] and dp[i] <=b:
-            cnt+=1
+    for v in fi:
+        if a <= v <= b:
+            cnt += 1
+        if b <= v:
+            break
     print(cnt)
