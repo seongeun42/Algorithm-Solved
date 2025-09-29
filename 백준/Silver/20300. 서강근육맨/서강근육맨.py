b4a40c726c2aa58915d4e2d65e89856195464fb6
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-09-29 22:14:41 +0900

    [Silver III] Title: 서강근육맨, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/20300. 서강근육맨/서강근육맨.py
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.py
@@ -3,16 +3,10 @@ input = sys.stdin.readline
 
 N = int(input())
 t = sorted([*map(int, input().split())])
-if N % 2 == 0:
-    M = t[0] + t[-1]
-    for i in range(1, N // 2):
-        v = t[i] + t[N - i - 1]
-        if M < v:
-            M = v
-else:
-    M = t[-1]
-    for i in range(N // 2):
-        v = t[i] + t[N - i - 2]
-        if M < v:
-            M = v
+odd = N % 2 == 1
+M = t[-1] if odd else 0
+for i in range(N // 2):
+    v = t[i] + (t[N - i - 2] if odd else t[N - i - 1])
+    if M < v:
+        M = v
 print(M)
