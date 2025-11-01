943117b35c49b84497f7d960cbad8322a3f2cf52
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-11-01 22:42:30 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 416 ms, Memory: 73068 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.py
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.py
@@ -1,18 +1,21 @@
 import sys, math
 input = sys.stdin.readline
 
-prefix = [[0] * 1001 for _ in range(1001)]
-for i in range(1001):
-    for j in range(1001):
-        if i == j == 0:
-            continue
-        elif i == 0 or j == 0:
-            prefix[i][j] = 1
-        else:
-            see = 1 if math.gcd(i, j) == 1 else 0
-            prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + see
+def solve():
+    C = int(input())
+    Q = [int(input()) for _ in range(C)]
+    max_q = max(Q) + 1
+    prefix = [[0] * max_q for _ in range(max_q)]
+    for i in range(max_q):
+        for j in range(max_q):
+            if i == j == 0:
+                continue
+            elif i == 0 or j == 0:
+                prefix[i][j] = 1
+            else:
+                see = 1 if math.gcd(i, j) == 1 else 0
+                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + see
+    for q in Q:
+        print(prefix[q][q])
 
-C = int(input())
-for _ in range(C):
-    N = int(input())
-    print(prefix[N][N])
+solve()
