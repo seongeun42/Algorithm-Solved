4d5fe3754cc065ed64ca2791b8c367fa2676c05b
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-11-07 15:25:35 +0900

    [Gold V] Title: 퇴사 2, Time: 2884 ms, Memory: 347744 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+TP = [[*map(int, input().split())] for _ in range(N)]
+dp = [0 for _ in range(N + 1)]
+for i in range(N - 1, -1, -1):
+    t, p = TP[i]
+    end = i + t
+    if end <= N:
+        dp[i] = max(dp[i + 1], p + dp[end])
+    else:
+        dp[i] = dp[i + 1]
+print(dp[0])
