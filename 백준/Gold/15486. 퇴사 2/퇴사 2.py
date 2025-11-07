ef249c0f9658b3a984efd3760dcbd68f045cb31d
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-11-07 15:32:06 +0900

    [Gold V] Title: 퇴사 2, Time: 1980 ms, Memory: 348768 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,14 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-TP = [[*map(int, input().split())] for _ in range(N)]
-dp = [0 for _ in range(N + 1)]
-for i in range(N - 1, -1, -1):
-    t, p = TP[i]
-    end = i + t
-    if end <= N and dp[i + 1] < p + dp[end]:
-        dp[i] = p + dp[end]
-    else:
-        dp[i] = dp[i + 1]
-print(dp[0])
+def solve():
+    N = int(input())
+    TP = [[*map(int, input().split())] for _ in range(N)]
+    dp = [0 for _ in range(N + 1)]
+    for i in range(N - 1, -1, -1):
+        t, p = TP[i]
+        end = i + t
+        if end <= N and dp[i + 1] < p + dp[end]:
+            dp[i] = p + dp[end]
+        else:
+            dp[i] = dp[i + 1]
+    print(dp[0])
+
+solve()
