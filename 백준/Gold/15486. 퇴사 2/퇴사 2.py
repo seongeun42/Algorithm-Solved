b76b549e1d9e6c9dbcb6cacf81d27951cc4c6eed
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-11-07 15:28:56 +0900

    [Gold V] Title: 퇴사 2, Time: 2716 ms, Memory: 347744 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -7,8 +7,8 @@ dp = [0 for _ in range(N + 1)]
 for i in range(N - 1, -1, -1):
     t, p = TP[i]
     end = i + t
-    if end <= N:
-        dp[i] = max(dp[i + 1], p + dp[end])
+    if end <= N and dp[i + 1] < p + dp[end]:
+        dp[i] = p + dp[end]
     else:
         dp[i] = dp[i + 1]
 print(dp[0])
