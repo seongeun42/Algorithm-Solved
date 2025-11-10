8891e5722d51ab57daec9fb8476e250092f9d182
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-11-10 11:35:55 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,17 +1,23 @@
-N = int(input())
-A = [*map(int, input().split())]
-desc = [1] * N
-asc = [1] * N
-desc[0], asc[-1] = 1, 1
-for i in range(1, N):
-    for j in range(i):
-        if A[j] < A[i] and desc[i] <= desc[j]:
-            desc[i] = desc[j] + 1
-        if A[N - j - 1] < A[N - i - 1] and asc[N - i - 1] <= asc[N - j - 1]:
-            asc[N - i - 1] = asc[N - j - 1] + 1
-ans = 0
-for i in range(N):
-    leng = desc[i] + asc[i]
-    if ans < leng:
-        ans = leng
-print(ans - 1)
+import sys
+input = sys.stdin.readline
+
+def solve():
+    N = int(input())
+    A = [*map(int, input().split())]
+    desc = [1] * N
+    asc = [1] * N
+    desc[0], asc[-1] = 1, 1
+    for i in range(1, N):
+        for j in range(i):
+            if A[j] < A[i] and desc[i] <= desc[j]:
+                desc[i] = desc[j] + 1
+            if A[N - j - 1] < A[N - i - 1] and asc[N - i - 1] <= asc[N - j - 1]:
+                asc[N - i - 1] = asc[N - j - 1] + 1
+    ans = 0
+    for i in range(N):
+        leng = desc[i] + asc[i]
+        if ans < leng:
+            ans = leng
+    print(ans - 1)
+
+solve()
