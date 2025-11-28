53b575bd3ab96683e4f905666f6909044346a0eb
Seongeun <64778589+seongeun42@users.noreply.github.com>
2025-11-28 12:20:49 +0900

    [Silver II] Title: 오리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.py
+++ 백준/Silver/12933. 오리/오리.py
@@ -1,33 +1,22 @@
-duck = input()
-visited = [False] * len(duck)
-cnt = 0
+def solve():
+    S = input()
+    if S[0] != 'q' or len(S) % 5 > 0:
+        print(-1)
+        exit()
+    quack = "quack"
+    visited = [False] * len(S)
+    cnt = 0
+    for i, c in enumerate(S):
+        if c == 'q' and not visited[i]:
+            si = 0
+            start = True
+            for j in range(i, len(S)):
+                if S[j] == quack[si] and not visited[j]:
+                    visited[j] = True
+                    if start and quack[si] == 'k':
+                        cnt += 1
+                        start = False
+                    si = (si + 1) % 5
+    print(-1 if not all(visited) or cnt == 0 else cnt)
 
-if len(duck) % 5 != 0:
-    print(-1)
-    exit()
-
-def solve(start):
-    global cnt
-    quack = 'quack'
-    j = 0
-    first = True
-    for i in range(start, len(duck)):
-        if duck[i] == quack[j] and not visited[i]:
-            visited[i] = True
-            if duck[i] == 'k':
-                if first:
-                    cnt += 1
-                    first = False
-                j = 0
-                continue
-            j += 1
-
-
-for i in range(len(duck)):
-    if duck[i] == 'q' and not visited[i]:
-        solve(i)
-
-if not all(visited) or cnt == 0:
-    print(-1)
-else:
-    print(cnt)
+solve()
