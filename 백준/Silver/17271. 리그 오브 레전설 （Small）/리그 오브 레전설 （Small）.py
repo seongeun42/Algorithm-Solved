b523dd59cdd8c01178188bb4caa029454be21758
Seongeun <64778589+seongeun42@users.noreply.github.com>
2026-04-05 21:42:03 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
@@ -1,11 +1,5 @@
 N, M = map(int, input().split())
-max_b = N // M
-MOD = 1_000_000_007
-factorial = [1]
-for i in range(1, N + 1):
-    factorial.append(factorial[i - 1] * i)
-ans = 0
-for i in range(max_b + 1):
-    a, b = N - (i * M), i
-    ans += (factorial[a + b] // (factorial[a] * factorial[b])) % MOD
-print(ans % MOD)
+dp = [1] * (N + 1)
+for i in range(M, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - M]) % 1_000_000_007
+print(dp[N])
