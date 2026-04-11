0524bbff2b58e50214a8259064d052481710f206
Seongeun <64778589+seongeun42@users.noreply.github.com>
2026-04-11 23:46:27 +0900

    [Silver IV] Title: 수 고르기, Time: 40 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20186. 수 고르기/수 고르기.py
+++ 백준/Silver/20186. 수 고르기/수 고르기.py
@@ -1,12 +1,3 @@
-from itertools import combinations
-
 N, K = map(int, input().split())
-nums = [*map(int, input().split())]
-ans = 0
-for cb in combinations(range(N), K):
-    v = 0
-    for i, idx in enumerate(cb):
-        v += nums[idx] - i
-    if ans < v:
-        ans = v
-print(ans)
+nums = sorted([*map(int, input().split())], reverse=True)
+print(sum(nums[:K]) - ((K - 1) * K // 2))
