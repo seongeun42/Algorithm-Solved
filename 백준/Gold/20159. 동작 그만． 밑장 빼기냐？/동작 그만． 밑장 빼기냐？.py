cc8048cc849677b2e9c8614ec3f359488ce56966
Seongeun <64778589+seongeun42@users.noreply.github.com>
2026-04-26 22:47:13 +0900

    [Gold IV] Title: 동작 그만. 밑장 빼기냐?, Time: 76 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.py
+++ 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.py
@@ -1,13 +1,19 @@
-N = int(input())
-card = [*map(int, input().split())]
-odd = [0, card[0]]
-even = [0]
-for i in range(2, N, 2):
-    odd.append(odd[-1] + card[i])
-    even.append(even[-1] + card[N - i - 1])
-even = even[::-1]
-ans = 0
-for i in range(N // 2):
-    ans = max(ans, odd[i] + card[-1] + even[i])
-    ans = max(ans, odd[i + 1] + even[i])
-print(ans)
+import sys
+input = sys.stdin.readline
+
+def solve():
+    N = int(input())
+    card = [*map(int, input().split())]
+    odd = [0, card[0]]
+    even = [0]
+    for i in range(2, N, 2):
+        odd.append(odd[-1] + card[i])
+        even.append(even[-1] + card[N - i - 1])
+    even = even[::-1]
+    ans = 0
+    for i in range(N // 2):
+        ans = max(ans, odd[i] + card[-1] + even[i])
+        ans = max(ans, odd[i + 1] + even[i])
+    print(ans)
+
+solve()
